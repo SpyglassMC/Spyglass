41cdc3b69e6971786d94a1446483829067484f14
SPGoding <SPGoding@outlook.com>
2019-11-04 21:18:37 +0800

    :green_heart: Fix CI

--- src/parsers/FunctionParser.ts
+++ src/parsers/FunctionParser.ts
@@ -1,48 +1,48 @@
-import Parser from '../types/Parser'
-import FunctionInfo from '../types/FunctionInfo'
-import LineParser from './LineParser'
+// import Parser from '../types/Parser'
+// import FunctionInfo from '../types/FunctionInfo'
+// import LineParser from './LineParser'
 
-/**
- * Parse input string as a function.
- */
-export default class FunctionParser implements Parser<FunctionInfo> {
-    parse(input: string): ParserResult {
-        const funcInfo: FunctionInfo = {
-            lines: []
-        }
+// /**
+//  * Parse input string as a function.
+//  */
+// export default class FunctionParser implements Parser<FunctionInfo> {
+//     parse(input: string): ParserResult {
+//         const funcInfo: FunctionInfo = {
+//             lines: []
+//         }
 
-        let isReadingDoc: boolean = true
-        for (const line of input.split('\n')) {
-            const { data: parsedLine } = new LineParser().parse(line)
-            funcInfo.lines.push(parsedLine)
-            //#region Load documentation
-            if (parsedLine.args[0].name !== 'CommentParser') {
-                isReadingDoc = false
-            }
-            if (isReadingDoc) {
-                funcInfo.doc = funcInfo.doc || ''
-                funcInfo.doc += `${line.slice(1)}\n`
-            }
-            //#endregion
-            //#region Load cache
-            if (parsedLine.cache &&
-                Object.keys(parsedLine.cache.def).length >= 1 &&
-                Object.keys(parsedLine.cache.ref).length >= 1) {
-                funcInfo.cache = funcInfo.cache || { def: {}, ref: {} }
-                funcInfo.cache = {
-                    def: { ...funcInfo.cache.def, ...parsedLine.cache.def },
-                    ref: { ...funcInfo.cache.ref, ...parsedLine.cache.ref }
-                }
-            }
-            //#endregion
-        }
+//         let isReadingDoc: boolean = true
+//         for (const line of input.split('\n')) {
+//             const { data: parsedLine } = new LineParser().parse(line)
+//             funcInfo.lines.push(parsedLine)
+//             //#region Load documentation
+//             if (parsedLine.args[0].name !== 'CommentParser') {
+//                 isReadingDoc = false
+//             }
+//             if (isReadingDoc) {
+//                 funcInfo.doc = funcInfo.doc || ''
+//                 funcInfo.doc += `${line.slice(1)}\n`
+//             }
+//             //#endregion
+//             //#region Load cache
+//             if (parsedLine.cache &&
+//                 Object.keys(parsedLine.cache.def).length >= 1 &&
+//                 Object.keys(parsedLine.cache.ref).length >= 1) {
+//                 funcInfo.cache = funcInfo.cache || { def: {}, ref: {} }
+//                 funcInfo.cache = {
+//                     def: { ...funcInfo.cache.def, ...parsedLine.cache.def },
+//                     ref: { ...funcInfo.cache.ref, ...parsedLine.cache.ref }
+//                 }
+//             }
+//             //#endregion
+//         }
 
-        return {
-            data: funcInfo
-        }
-    }
-}
+//         return {
+//             data: funcInfo
+//         }
+//     }
+// }
 
-type ParserResult = {
-    data: FunctionInfo
-}
+// type ParserResult = {
+//     data: FunctionInfo
+// }
