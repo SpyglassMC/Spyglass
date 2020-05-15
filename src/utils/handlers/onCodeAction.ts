40e33d35b5dadfacc865f62311365b094fb13b73
SPGoding <SPGoding@outlook.com>
2020-05-14 21:28:10 -0500

    🐛 Fix Request textDocument/codeAction failed
    Fix #467.

--- src/utils/handlers/onCodeAction.ts
+++ src/utils/handlers/onCodeAction.ts
@@ -10,19 +10,22 @@ export function onCodeAction({ uri, info, diagnostics, range }: { uri: Uri, info
     for (let i = range.start.line; i <= range.end.line; i++) {
         const line = info.lines[i]
         const selectedRange = { start: range.start.character, end: range.end.character }
-
-        for (const { data } of line.args) {
-            if (data instanceof ArgumentNode) {
-                const nodeRange = data[NodeRange]
-                if (areOverlapped(selectedRange, nodeRange)) {
-                    const diagnosticsMap: DiagnosticMap = {}
-                    for (const diag of diagnostics) {
-                        if (diag.code !== undefined) {
-                            diagnosticsMap[diag.code as ErrorCode] = diagnosticsMap[diag.code as ErrorCode] || []
-                            diagnosticsMap[diag.code as ErrorCode]!.push(diag)
+        
+        /* istanbul ignore else */
+        if (line) {
+            for (const { data } of line.args) {
+                if (data instanceof ArgumentNode) {
+                    const nodeRange = data[NodeRange]
+                    if (areOverlapped(selectedRange, nodeRange)) {
+                        const diagnosticsMap: DiagnosticMap = {}
+                        for (const diag of diagnostics) {
+                            if (diag.code !== undefined) {
+                                diagnosticsMap[diag.code as ErrorCode] = diagnosticsMap[diag.code as ErrorCode] || []
+                                diagnosticsMap[diag.code as ErrorCode]!.push(diag)
+                            }
                         }
+                        ans.push(...data[GetCodeActions](uri.toString(), info, i, selectedRange, diagnosticsMap))
                     }
-                    ans.push(...data[GetCodeActions](uri.toString(), info, i, selectedRange, diagnosticsMap))
                 }
             }
         }
