2c641e06e69a6d4e54d3bbf92442d9326f7e2c99
SPGoding <SPGoding@outlook.com>
2020-06-23 18:48:25 -0500

    Duck

--- src/test/parsers/EntityArgumentParser.spec.ts
+++ src/test/parsers/EntityArgumentParser.spec.ts
@@ -223,18 +223,11 @@ describe('EntityArgumentParser Tests', () => {
             })
             it('Should return data with all kinds of negativable array arguments', () => {
                 const parser = new EntityArgumentParser('multiple', 'entities')
-                const reader = new StringReader('@e[gamemode=adventure,name=!SPGoding,predicate=spgoding:test/predicate,tag=foo,team=!red,type=#spgoding:mobs,nbt={NoGravity: 1b}]')
+                const reader = new StringReader('@e[gamemode=adventure,name=!SPGoding,predicate=spgoding:test/predicate,tag=foo,team=!red,type=#spgoding:mobs,nbt={}]')
 
                 const expectedCompound = new NbtCompoundNode(null)
-                expectedCompound[NodeRange] = { start: 113, end: 122 }
-                const expectedCompoundKey = new NbtCompoundKeyNode(expectedCompound, 'NoGravity', 'NoGravity', { start: 114 })
-                expectedCompoundKey[NodeRange] = { start: 114, end: 123 }
-                expectedCompound[Keys].NoGravity = expectedCompoundKey
-                const expectedByte = new NbtByteNode(expectedCompound, 1, '1')
-                expectedByte[NodeRange] = { start: 125, end: 127 }
-                expectedCompound.NoGravity = expectedByte
-                expectedCompound[UnsortedKeys].push('NoGravity')
-                const expected = $(new SelectorArgumentsNode(), [2, 129], {
+                expectedCompound[NodeRange] = { start: 113, end: 115 }
+                const expected = $(new SelectorArgumentsNode(), [2, 116], {
                     gamemode: ['adventure'],
                     nameNeg: [$(new StringNode('SPGoding', 'SPGoding', { start: 28 }), [28, 36])],
                     predicate: [$(new IdentityNode('spgoding', ['test', 'predicate']), [47, 70])],
@@ -255,8 +248,8 @@ describe('EntityArgumentParser Tests', () => {
                 })
 
                 const actual = parser.parse(reader, ctx)
-
-                assert.deepStrictEqual(actual.data, $(new EntityNode(undefined, 'e', expected), [0, 129]))
+                
+                assert.deepStrictEqual(actual.data, $(new EntityNode(undefined, 'e', expected), [0, 116]))
                 assert.deepStrictEqual(actual.errors, [])
             })
             it('Should return data with the same negativable array arguments', () => {
