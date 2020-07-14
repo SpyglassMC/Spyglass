411fd9e2f057c06ea0b290c06474eba0a210dcc8
SPGoding <SPGoding@outlook.com>
2020-07-14 13:27:47 -0500

    ♻ Better datapack detection (#595)
    * 👥 Update contributors
    
    * ♻️ Improve data pack root detection feature
    
    Fix #593. Resolve #445.
    
    * ✅ Fix ClientCache test

--- src/test/types/ClientCache.spec.ts
+++ src/test/types/ClientCache.spec.ts
@@ -2,7 +2,7 @@ import assert = require('power-assert')
 import { describe, it } from 'mocha'
 import { MarkupKind } from 'vscode-languageserver'
 import { URI as Uri } from 'vscode-uri'
-import { ClientCache, combineCache, getCacheFromOffset, getCompletions, getSafeCategory, isDefinitionType, isFileType, isNamespacedType, isTagType, remapCachePosition, removeCachePosition, removeCacheUnit, trimCache } from '../../types/ClientCache'
+import { ClientCache, combineCache, getCacheFromOffset, getCompletions, getSafeCategory, isDefinitionType, isFileType, isNamespacedType, isTagRegularFileType, remapCachePosition, removeCachePosition, removeCacheUnit, trimCache } from '../../types/ClientCache'
 import { assertCompletions } from '../utils.spec'
 
 describe('ClientCache Tests', () => {
@@ -293,13 +293,13 @@ describe('ClientCache Tests', () => {
             assert.deepStrictEqual(actual, {})
         })
     })
-    describe('isTagType() Tests', () => {
+    describe('isTagRegularFileType() Tests', () => {
         it('Should return true', () => {
-            const actual = isTagType('tag/block')
+            const actual = isTagRegularFileType('tag/block')
             assert(actual === true)
         })
         it('Should return false', () => {
-            const actual = isTagType('advancement')
+            const actual = isTagRegularFileType('advancement')
             assert(actual === false)
         })
     })
