2e91325aca77bf0293ee7e1ba4085d8a3a5a74e8
SPGoding <SPGoding@outlook.com>
2020-07-13 22:58:57 -0500

    🐛 Remove vector range limit from dust particles
    Fix #576.

--- src/parsers/ParticleArgumentParser.ts
+++ src/parsers/ParticleArgumentParser.ts
@@ -36,9 +36,7 @@ export class ParticleArgumentParser extends ArgumentParser<ParticleNode<any>> {
                         .expect(' ')
                         .skip()
                     const colorResult: ArgumentParserResult<VectorNode> = ctx.parsers.get('Vector', [
-                        3, 'float', false, false,
-                        [0, 0, 0],
-                        [1, 1, 1]
+                        3, 'float', false, false
                     ]).parse(reader, ctx)
                     const color = colorResult.data
                     combineArgumentParserResult(ans, colorResult)
