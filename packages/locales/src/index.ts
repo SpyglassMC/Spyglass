98cc1b5660b92b42ee563dba7f5d5f0dbfd5ca2e
SPGoding <15277496+SPGoding@users.noreply.github.com>
2020-12-20 17:45:24 -0600

    ➕ Add @spyglassmc/locales
    [skip ci]

---
+++ packages/locales/src/index.ts
@@ -0,0 +1,78 @@
+import Fallback from './en.json'
+import McschemaFallback from '@mcschema/locales/src/en.json'
+
+type Locale = Record<string, string>
+
+const Locales: Record<string, Locale> = {
+    en: { ..._addPrefix(McschemaFallback, 'json'), ...Fallback }
+}
+
+let language = 'en'
+
+export function localize(key: string, params?: any[]): string | undefined
+export function localize(segments: string[], params?: any[], depth?: number, minDepth?: number): string | undefined
+export function localize(param0: string | string[], params: any[] = [], depth = 5, minDepth = 1): string | undefined {
+    if (typeof param0 === 'string') {
+        return _localize(param0, params)
+    }
+    return _segmentedLocalize(param0, params, depth, minDepth)
+}
+
+export async function loadLocale(
+    setting: string,
+    defaultLocaleCode = 'en'
+) {
+    const specifiedLanguage = setting.toLowerCase() === 'default' ? defaultLocaleCode : setting
+    if (specifiedLanguage !== language) {
+        return _setupLanguage(specifiedLanguage)
+    }
+}
+
+function _localize(key: string, params: any[]) {
+    const value: string | undefined = Locales[language][key] ?? Locales.en[key]
+
+    return _resolveLocalePlaceholders(value, params) ?? (
+        console.error(new Error(`Unknown locale key “${key}”`)),
+        ''
+    )
+}
+
+function _resolveLocalePlaceholders(val: string | undefined, params: string[]) {
+    return val?.replace(/%\d+%/g, match => {
+        const index = parseInt(match.slice(1, -1))
+        return params?.[index] !== undefined ? params[index] : match
+    })
+}
+
+function _segmentedLocalize(segments: string[], params: string[], depth: number, minDepth: number): string | undefined {
+    return [language, 'en'].reduce((prev, code) => {
+        if (prev !== undefined) return prev
+
+        const array = segments.slice(-depth)
+        while (array.length >= minDepth) {
+            const locale = _resolveLocalePlaceholders(Locales[code][array.join('.')], params)
+            if (locale !== undefined) return locale
+            array.shift()
+        }
+
+        return undefined
+    }, undefined)
+}
+
+async function _setupLanguage(code: string) {
+    const locale = await import(`./${code}.json`)
+    const jsonLocale = await import(`@mcschema/locales/src/${code}.json`)
+    Locales[code] = { ..._addPrefix(jsonLocale, 'json'), ...locale }
+    language = code
+
+    console.info(`[I18N] Set to “${code}”.`)
+}
+
+
+function _addPrefix(locale: Locale, prefix: string) {
+    const ans: Locale = {}
+    for (const key of Object.keys(locale)) {
+        ans[`${prefix}.${key}`] = locale[key]
+    }
+    return ans
+}
