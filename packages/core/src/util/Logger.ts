da0620405ee090dd6a834cda8ca2337294ea85de
SPGoding <15277496+SPGoding@users.noreply.github.com>
2021-02-23 20:08:47 -0600

    🏷️ Update the Logger interface

--- packages/core/src/util/Logger.ts
+++ packages/core/src/util/Logger.ts
@@ -4,25 +4,31 @@ export interface Logger {
 	/**
 	 * Log a message.
 	 */
-	log(message: any): void
+	log(message: string): void
+
+	/**
+	 * Log an information.
+	 */
+	info(message: string): void
 
 	/**
 	 * Output a warning message.
 	 */
-	warn(message: any): void
+	warn(message: string): void
 
 	/**
 	 * Output an error message.
 	 */
-	error(message: any): void
+	error(message: string): void
 }
 
 export namespace Logger {
 	let instance: Logger | undefined
 
 	/**
-	 * @returns A `LogService` implementation that provides access to the real file system
-	 * based on the build-in node.js `console`.
+	 * @returns A `Logger` implementation based on the built-in `console` instance.
+	 * Do **not** use this implementation in language servers, as some clients cannot handle
+	 * non-LSP stdout. (https://github.com/SPYGlassMC/SPYGlass/issues/845)
 	 */
 	export function create(): Logger {
 		return instance ?? (instance = new LogServiceImpl())
@@ -30,13 +36,16 @@ export namespace Logger {
 }
 
 class LogServiceImpl implements Logger {
-	log(message: any): void {
+	log(message: string): void {
 		console.log(message)
 	}
-	warn(message: any): void {
+	info(message: string): void {
+		console.info(message)
+	}
+	warn(message: string): void {
 		console.warn(message)
 	}
-	error(message: any): void {
+	error(message: string): void {
 		console.error(message)
 	}
 }
