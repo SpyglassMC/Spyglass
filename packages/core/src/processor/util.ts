185bcdd9621da6a033bef02ccda34834759f6b0b
SPGoding <15277496+SPGoding@users.noreply.github.com>
2021-02-26 14:47:16 -0600

    🐛 Remove a debugging console.log
    Fix #851. [skip ci]

--- packages/core/src/processor/util.ts
+++ packages/core/src/processor/util.ts
@@ -1,7 +1,6 @@
 import { AstNode } from '../node'
 
 export function traverseLeaves(node: AstNode, fn: (this: void, leaf: AstNode) => unknown) {
-	console.log(`Traverse ${JSON.stringify(node)}`)
 	if (node.children) {
 		for (const child of node.children) {
 			traverseLeaves(child, fn)
