ea0ce257f68ed83b3b633ec942722b87306d103e
SPGoding <15277496+SPGoding@users.noreply.github.com>
2021-02-27 17:45:46 -0600

    🔥 Remove LoggerImpl
    The type Console is compatible with Logger directly, so we don't have to wrap around it.

--- packages/core/src/util/Logger.ts
+++ packages/core/src/util/Logger.ts
@@ -23,29 +23,12 @@ export interface Logger {
 }
 
 export namespace Logger {
-	let instance: Logger | undefined
-
 	/**
-	 * @returns A `Logger` implementation based on the built-in `console` instance.
+	 * @returns The built-in `console`.
 	 * Do **not** use this implementation in language servers, as some clients cannot handle
 	 * non-LSP stdout. (https://github.com/SPYGlassMC/SPYGlass/issues/845)
 	 */
 	export function create(): Logger {
-		return instance ?? (instance = new LogServiceImpl())
-	}
-}
-
-class LogServiceImpl implements Logger {
-	log(message: string): void {
-		console.log(message)
-	}
-	info(message: string): void {
-		console.info(message)
-	}
-	warn(message: string): void {
-		console.warn(message)
-	}
-	error(message: string): void {
-		console.error(message)
+		return console
 	}
 }
