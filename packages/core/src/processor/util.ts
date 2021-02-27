a1fe84a338b9871d073bbe15cf2177110993c8fb
SPGoding <15277496+SPGoding@users.noreply.github.com>
2021-02-27 15:52:39 -0600

    ✨ Add parents to traverseLeaves' callback

--- packages/core/src/processor/util.ts
+++ packages/core/src/processor/util.ts
@@ -1,11 +1,35 @@
 import { AstNode } from '../node'
 
-export function traverseLeaves(node: AstNode, fn: (this: void, leaf: AstNode) => unknown) {
+export function traverseLeaves(node: AstNode, fn: (this: void, leaf: AstNode, parents: AstNode[]) => unknown, /** @internal */ existingParents: AstNode[] = []): void {
 	if (node.children) {
 		for (const child of node.children) {
-			traverseLeaves(child, fn)
+			existingParents.push(node)
+			traverseLeaves(child, fn, existingParents)
+			existingParents.pop()
 		}
 	} else {
-		fn(node)
+		fn(node, existingParents)
 	}
 }
+
+// mASteRPiecE /s
+// export function traverseLeaves(node: AstNode, fn: (this: void, leaf: AstNode, parents: AstNode[]) => unknown): void {
+// 	// LIFO, but things are inserted at the beginning.
+// 	const stack: AstNode[] = [node]
+// 	const visitedIndices: number[] = [-1]
+// 	while (stack.length) {
+// 		const top = stack[stack.length - 1]
+// 		let visitedIndex = visitedIndices[stack.length - 1]
+// 		if (!top.children || visitedIndex === top.children.length - 1) {
+// 			if (!top.children) {
+// 				fn(top, stack)
+// 			}
+// 			stack.shift()
+// 			visitedIndices.shift()
+// 		} else {
+// 			visitedIndex = visitedIndices[stack.length - 1] += 1
+// 			stack.unshift(top.children[visitedIndex])
+// 			visitedIndices.unshift(-1)
+// 		}
+// 	}
+// }
