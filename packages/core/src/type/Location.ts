1d21afbdfcf683f9b380789950e873346b3e4882
SPGoding <15277496+SPGoding@users.noreply.github.com>
2021-02-27 17:30:55 -0600

    🏷️ Add Location#get

--- packages/core/src/type/Location.ts
+++ packages/core/src/type/Location.ts
@@ -1,15 +1,21 @@
-import { Range } from './Range'
+import { Range, RangeLike } from './Range'
 
 export interface Location {
 	uri: string,
 	range: Range
 }
 
+export type LocationLike = Partial<{ uri: string, range: RangeLike }>
+
 export namespace Location {
-	export function create(partial: Partial<Location>): Location {
+	export function get(partial: LocationLike): Location {
 		return {
 			uri: partial.uri ?? '',
 			range: Range.get(partial.range ?? { start: 0, end: 0 }),
 		}
+	}
+
+	export function create(uri: string, range: RangeLike): Location {
+		return Location.get({ uri, range })
 	}
 }
--- packages/core/test/type/Location.spec.ts
+++ packages/core/test/type/Location.spec.ts
@@ -4,20 +4,28 @@ import snapshot from 'snap-shot-it'
 import { Location, Range } from '../../lib'
 
 describe('Location', () => {
-	describe('create()', () => {
+	describe('get()', () => {
 		it('Should create correctly', () => {
-			snapshot(Location.create({}))
-			snapshot(Location.create({ uri: 'file:///home/spgoding/test' }))
-			snapshot(Location.create({ range: Range.create(1, 2) }))
-			snapshot(Location.create({ uri: 'file:///home/spgoding/test', range: Range.create(1, 2) }))
+			snapshot(Location.get({}))
+			snapshot(Location.get({ uri: 'file:///home/spgoding/test' }))
+			snapshot(Location.get({ range: Range.create(1, 2) }))
+			snapshot(Location.get({ uri: 'file:///home/spgoding/test', range: Range.create(1, 2) }))
+			snapshot(Location.get({ uri: 'file:///home/spgoding/test', range: () => ({ range: Range.create(3, 4) }) }))
 		})
 		it('Should create a new object from the passed-in Position', () => {
-			const incoming = Location.create({ range: Range.create(1, 2) })
+			const incoming = Location.get({ range: Range.create(1, 2) })
 
-			const result = Location.create(incoming)
+			const result = Location.get(incoming)
 			incoming.range = Range.create(9, 9)
 
-			assert.deepStrictEqual(result, Location.create({ range: Range.create(1, 2) }))
+			assert.deepStrictEqual(result, Location.get({ range: Range.create(1, 2) }))
+		})
+	})
+	describe('create()', () => {
+		it('Should create correctly', () => {
+			snapshot(Location.create('file:///home/spgoding/test', Range.create(5, 6)))
+			snapshot(Location.create('file:///home/spgoding/test', { range: Range.create(7, 8) }))
+			snapshot(Location.create('file:///home/spgoding/test', 9))
 		})
 	})
 })
