7de1701494f550cd99638bbbaf351d33e33fa1e3
Misode <Misoloo64@gmail.com>
2021-02-28 03:40:02 +0100

    :globe_with_meridians: Localize json errors

--- packages/json/src/checker/primitives/boolean.ts
+++ packages/json/src/checker/primitives/boolean.ts
@@ -1,8 +1,9 @@
+import { localize } from '@spyglassmc/locales'
 import { JsonAstNode, JsonBooleanAstNode } from '../../node'
 import { CheckerContext } from '../CheckerContext'
 
 export function boolean(node: JsonAstNode, ctx: CheckerContext) {
 	if (!JsonBooleanAstNode.is(node)) {
-		ctx.err.report('Expected a boolean', node)
+		ctx.err.report(localize('expected', [localize('boolean')]), node)
 	}
 }
---
+++ packages/json/src/checker/primitives/number.ts
@@ -0,0 +1,37 @@
+import { localize } from '@spyglassmc/locales'
+import { JsonAstNode, JsonNumberAstNode } from '../../node'
+import { CheckerContext } from '../CheckerContext'
+
+export function int(node: JsonAstNode, ctx: CheckerContext): node is JsonNumberAstNode {
+	if (!JsonNumberAstNode.is(node) || !node.isInteger) {
+		ctx.err.report(localize('expected', [localize('integer')]), node)
+		return false
+	}
+	return true
+}
+
+export function float(node: JsonAstNode, ctx: CheckerContext): node is JsonNumberAstNode {
+	if (!JsonNumberAstNode.is(node)) {
+		ctx.err.report(localize('expected', [localize('float')]), node)
+		return false
+	}
+	return true
+}
+
+const range = (checker: (node: JsonAstNode, ctx: CheckerContext) => node is JsonNumberAstNode) => (min: number | null, max: number | null) => {
+	return (node: JsonAstNode, ctx: CheckerContext) => {
+		if (checker(node, ctx)) {
+			if (min !== null && max !== null && (min < node.value || max > node.value)) {
+				ctx.err.report(localize('expected', [localize('number.between', [min, max])]), node)
+			} else if (min !== null && node.value < min) {
+				ctx.err.report(localize('expected', [localize('number.>=', [min])]), node)
+			} else if (max !== null && node.value > max) {
+				ctx.err.report(localize('expected', [localize('number.<=', [max])]), node)
+			}
+		}
+	}
+}
+
+export const intRange = range(float)
+
+export const floatRange = range(float)
