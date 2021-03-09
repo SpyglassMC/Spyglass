7e1f4f8e729895cf77a221fa1b05a08e3aa67cab
SPGoding <15277496+SPGoding@users.noreply.github.com>
2021-03-08 18:11:53 -0600

    🔧 Support node.js addons in webpack

--- packages/vscode-extension/webpack.config.js
+++ packages/vscode-extension/webpack.config.js
@@ -8,37 +8,40 @@ const path = require('path')
  * @type {import('webpack').Configuration}
  */
 module.exports = {
-    target: 'node',
-    mode: 'production',
+	target: 'node',
+	node: {
+		__dirname: false,
+	},
+	mode: 'production',
 
-    entry: {
-        extension: './src/extension.ts',
-        server: '../language-server/lib/server.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        libraryTarget: 'commonjs2',
-        devtoolModuleFilenameTemplate: '../[resource-path]'
-    },
-    devtool: 'source-map',
-    externals: {
-        vscode: 'commonjs vscode'
-    },
-    resolve: {
-        extensions: ['.ts', '.js', 'json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'ts-loader'
-                    }
-                ]
-            }
-        ]
-    }
+	entry: {
+		extension: './src/extension.ts',
+		server: '../language-server/lib/server.js'
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+		libraryTarget: 'commonjs2',
+		devtoolModuleFilenameTemplate: '../[resource-path]'
+	},
+	devtool: 'source-map',
+	externals: {
+		vscode: 'commonjs vscode'
+	},
+	resolve: {
+		extensions: ['.ts', '.js', 'json']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
+			{
+				test: /\.node$/,
+				loader: 'node-loader',
+			},
+		]
+	}
 }
