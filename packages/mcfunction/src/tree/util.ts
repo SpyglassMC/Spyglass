c501b797d3ea8d608cd50a13128f496cddec6372
SPGoding <15277496+SPGoding@users.noreply.github.com>
2021-07-11 16:07:26 -0500

    🐛 Fix tree patches not being applied

--- packages/mcfunction/src/tree/util.ts
+++ packages/mcfunction/src/tree/util.ts
@@ -1,18 +1,19 @@
+import rfdc from 'rfdc'
 import type { TreeNode } from './type'
 
-function isObject(value: any): value is Exclude<object, Array<any>> {
-	return value && typeof value === 'object' && !Array.isArray(value)
+function isObject(value: unknown): value is Exclude<object, Array<any>> {
+	return !!value && typeof value === 'object' && !Array.isArray(value)
 }
 
 export function merge<T extends Record<string, any>>(a: T, b: Record<string, any>): T {
-	const ans = JSON.parse(JSON.stringify(a))
-	for (const [key, value] of Object.entries(b)) {
+	const ans = rfdc()(a)
+	for (const [key, value] of Object.entries(b) as [keyof typeof ans, any][]) {
 		if (isObject(ans[key]) && isObject(value)) {
-			merge(ans[key], value)
+			ans[key] = merge(ans[key], value)
 		} else if (value === undefined) {
 			delete ans[key]
 		} else {
-			ans[key] === value
+			ans[key] = value
 		}
 	}
 	return ans
