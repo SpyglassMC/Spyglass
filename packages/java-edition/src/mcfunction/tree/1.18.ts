a8289312a335c040a1bc4d193c5e8064ef95b033
SPGoding <15277496+SPGoding@users.noreply.github.com>
2022-01-21 20:16:59 -0600

    🐛 Update 1.18 patch for locate and placefeature

--- packages/java-edition/src/mcfunction/tree/1.18.ts
+++ packages/java-edition/src/mcfunction/tree/1.18.ts
@@ -1,3 +1,24 @@
+import type { PartialRootTreeNode } from '@spyglassmc/mcfunction'
+import { merge } from '@spyglassmc/mcfunction'
 import { Tree1_17 } from './1.17'
 
-export const Tree1_18 = Tree1_17
+export const Tree1_18: PartialRootTreeNode = merge(Tree1_17, {
+	locate: {
+		children: {
+			structure: {
+				properties: {
+					category: 'worldgen/structure_feature',
+				},
+			},
+		},
+	},
+	placefeature: {
+		children: {
+			feature: {
+				properties: {
+					category: 'worldgen/configured_feature',
+				},
+			},
+		},
+	},
+})
