ed32b87d1a35f630defd158de3ebedc1a4df4748
SPGoding <15277496+SPGoding@users.noreply.github.com>
2022-03-29 21:44:01 -0500

    ♻️ Replace any in main JSON parser with select

--- packages/json/src/parser/entry.ts
+++ packages/json/src/parser/entry.ts
@@ -7,15 +7,16 @@ import { number } from './number'
 import { object } from './object'
 import { string } from './string'
 
+const LegalNumberStart = new Set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'])
 export function json(dumpErrors = false): core.Parser<JsonNode> {
 	return (src, ctx) => {
-		const result = core.any([
-			string,
-			number,
-			boolean,
-			null_,
-			object,
-			array,
+		const result = core.select([
+			{ predicate: src => src.tryPeek('['), parser: array },
+			{ predicate: src => src.tryPeek('false') || src.tryPeek('true'), parser: boolean },
+			{ predicate: src => src.tryPeek('null'), parser: null_ },
+			{ predicate: src => LegalNumberStart.has(src.peek()), parser: number },
+			{ predicate: src => src.tryPeek('{'), parser: object },
+			{ parser: string },
 		])(src, ctx)
 
 		if (dumpErrors) {
