953629a7d44d59fc11de13d368fa8e5b20512c2a
SPGoding <15277496+SPGoding@users.noreply.github.com>
2022-03-29 21:16:02 -0500

    🩹 Fix not catching errors for some requests

--- packages/core/src/service/Service.ts
+++ packages/core/src/service/Service.ts
@@ -64,40 +64,58 @@ export class Service extends EventEmitter {
 	}
 
 	colorize(node: FileNode<AstNode>, doc: TextDocument, range?: Range): readonly ColorToken[] {
-		this.debug(`Colorizing '${doc.uri}' # ${doc.version}`)
-		const colorizer = this.project.meta.getColorizer(node.type)
-		return colorizer(node, ColorizerContext.create(this.project, { doc, range }))
+		try {
+			this.debug(`Colorizing '${doc.uri}' # ${doc.version}`)
+			const colorizer = this.project.meta.getColorizer(node.type)
+			return colorizer(node, ColorizerContext.create(this.project, { doc, range }))
+		} catch (e) {
+			this.logger.error(`[Service] [colorize] Failed for “${doc.uri}” #${doc.version}`, e)
+		}
+		return []
 	}
 
 	getColorInfo(node: FileNode<AstNode>, doc: TextDocument): ColorInfo[] {
-		this.debug(`Getting color info for '${doc.uri}' # ${doc.version}`)
-		const ans: ColorInfo[] = []
-		traversePreOrder(node, _ => true, node => node.color, node => ans.push({
-			color: Array.isArray(node.color) ? node.color : node.color!.value,
-			range: Array.isArray(node.color) ? node.range : (node.color!.range ?? node.range),
-		}))
-		return ans
+		try {
+			this.debug(`Getting color info for '${doc.uri}' # ${doc.version}`)
+			const ans: ColorInfo[] = []
+			traversePreOrder(node, _ => true, node => node.color, node => ans.push({
+				color: Array.isArray(node.color) ? node.color : node.color!.value,
+				range: Array.isArray(node.color) ? node.range : (node.color!.range ?? node.range),
+			}))
+			return ans
+		} catch (e) {
+			this.logger.error(`[Service] [getColorInfo] Failed for “${doc.uri}” #${doc.version}`, e)
+		}
+		return []
 	}
 
 	getColorPresentation(file: FileNode<AstNode>, doc: TextDocument, range: Range, color: Color): ColorPresentation[] {
-		this.debug(`Getting color presentation for '${doc.uri}' # ${doc.version} @ ${Range.toString(range)}`)
-		let node = AstNode.findDeepestChild({ node: file, needle: range.start })
-		while (node) {
-			const nodeColor = node.color
-			if (nodeColor && !Array.isArray(nodeColor)) {
-				const colorRange = nodeColor.range ?? node.range
-				return nodeColor.format.map(format => ColorPresentation.fromColorFormat(format, color, colorRange))
+		try {
+			this.debug(`Getting color presentation for '${doc.uri}' # ${doc.version} @ ${Range.toString(range)}`)
+			let node = AstNode.findDeepestChild({ node: file, needle: range.start })
+			while (node) {
+				const nodeColor = node.color
+				if (nodeColor && !Array.isArray(nodeColor)) {
+					const colorRange = nodeColor.range ?? node.range
+					return nodeColor.format.map(format => ColorPresentation.fromColorFormat(format, color, colorRange))
+				}
+				node = node.parent
 			}
-			node = node.parent
+		} catch (e) {
+			this.logger.error(`[Service] [getColorPresentation] Failed for “${doc.uri}” #${doc.version}`, e)
 		}
 		return []
 	}
 
 	complete(node: FileNode<AstNode>, doc: TextDocument, offset: number, triggerCharacter?: string) {
-		this.debug(`Getting completion for '${doc.uri}' # ${doc.version} @ ${offset}`)
-		const shouldComplete = this.project.meta.shouldComplete(doc.languageId, triggerCharacter)
-		if (shouldComplete) {
-			return completer.file(node, CompleterContext.create(this.project, { doc, offset, triggerCharacter }))
+		try {
+			this.debug(`Getting completion for '${doc.uri}' # ${doc.version} @ ${offset}`)
+			const shouldComplete = this.project.meta.shouldComplete(doc.languageId, triggerCharacter)
+			if (shouldComplete) {
+				return completer.file(node, CompleterContext.create(this.project, { doc, offset, triggerCharacter }))
+			}
+		} catch (e) {
+			this.logger.error(`[Service] [complete] Failed for “${doc.uri}” #${doc.version}`, e)
 		}
 		return []
 	}
@@ -122,41 +140,54 @@ export class Service extends EventEmitter {
 	}
 
 	getHover(file: FileNode<AstNode>, doc: TextDocument, offset: number): Hover | undefined {
-		this.debug(`Getting hover for '${doc.uri}' # ${doc.version} @ ${offset}`)
-		let node = AstNode.findDeepestChild({ node: file, needle: offset })
-		while (node) {
-			const symbol = this.project.symbols.resolveAlias(node.symbol)
-			if (symbol) {
-				const hover = `\`\`\`typescript\n(${symbol.category}${symbol.subcategory ? `/${symbol.subcategory}` : ''}) ${symbol.identifier}\n\`\`\`` + (symbol.desc ? `\n******\n${symbol.desc}` : '')
-				return Hover.create(node.range, hover)
+		try {
+			this.debug(`Getting hover for '${doc.uri}' # ${doc.version} @ ${offset}`)
+			let node = AstNode.findDeepestChild({ node: file, needle: offset })
+			while (node) {
+				const symbol = this.project.symbols.resolveAlias(node.symbol)
+				if (symbol) {
+					const hover = `\`\`\`typescript\n(${symbol.category}${symbol.subcategory ? `/${symbol.subcategory}` : ''}) ${symbol.identifier}\n\`\`\`` + (symbol.desc ? `\n******\n${symbol.desc}` : '')
+					return Hover.create(node.range, hover)
+				}
+				if (node.hover) {
+					return Hover.create(node.range, node.hover)
+				}
+				node = node.parent
 			}
-			if (node.hover) {
-				return Hover.create(node.range, node.hover)
-			}
-			node = node.parent
+		} catch (e) {
+			this.logger.error(`[Service] [getHover] Failed for “${doc.uri}” #${doc.version}`, e)
 		}
 		return undefined
 	}
 
 	getInlayHints(node: FileNode<AstNode>, doc: TextDocument, range?: Range): InlayHint[] {
-		// TODO: `range` argument is not used.
-		this.debug(`Getting inlay hints for '${doc.uri}' # ${doc.version}`)
-		const ans: InlayHint[] = []
-		const ctx = ProcessorContext.create(this.project, { doc })
-		for (const provider of this.project.meta.inlayHintProviders) {
-			ans.push(...provider(node, ctx))
+		try {
+			// TODO: `range` argument is not used.
+			this.debug(`Getting inlay hints for '${doc.uri}' # ${doc.version}`)
+			const ans: InlayHint[] = []
+			const ctx = ProcessorContext.create(this.project, { doc })
+			for (const provider of this.project.meta.inlayHintProviders) {
+				ans.push(...provider(node, ctx))
+			}
+			return ans
+		} catch (e) {
+			this.logger.error(`[Service] [getInlayHints] Failed for “${doc.uri}” #${doc.version}`, e)
 		}
-		return ans
+		return []
 	}
 
 	getSignatureHelp(node: FileNode<AstNode>, doc: TextDocument, offset: number): SignatureHelp | undefined {
-		this.debug(`Getting signature help for '${doc.uri}' # ${doc.version} @ ${offset}`)
-		const ctx = SignatureHelpProviderContext.create(this.project, { doc, offset })
-		for (const provider of this.project.meta.signatureHelpProviders) {
-			const result = provider(node, ctx)
-			if (result) {
-				return result
+		try {
+			this.debug(`Getting signature help for '${doc.uri}' # ${doc.version} @ ${offset}`)
+			const ctx = SignatureHelpProviderContext.create(this.project, { doc, offset })
+			for (const provider of this.project.meta.signatureHelpProviders) {
+				const result = provider(node, ctx)
+				if (result) {
+					return result
+				}
 			}
+		} catch (e) {
+			this.logger.error(`[Service] [getSignatureHelp] Failed for “${doc.uri}” #${doc.version}`, e)
 		}
 		return undefined
 	}
@@ -168,29 +199,38 @@ export class Service extends EventEmitter {
 	 * @returns Symbol locations of the selected symbol at `offset`, or `undefined` if there's no symbol at `offset`.
 	 */
 	getSymbolLocations(file: FileNode<AstNode>, doc: TextDocument, offset: number, searchedUsages: readonly SymbolUsageType[] = SymbolUsageTypes, currentFileOnly = false): SymbolLocations | undefined {
-		this.debug(`Getting symbol locations of usage '${searchedUsages.join(',')}' for '${doc.uri}' # ${doc.version} @ ${offset} with currentFileOnly=${currentFileOnly}`)
-		let node = AstNode.findDeepestChild({ node: file, needle: offset })
-		while (node) {
-			const symbol = this.project.symbols.resolveAlias(node.symbol)
-			if (symbol) {
-				const locations: SymbolLocation[] = []
-				for (const usage of searchedUsages) {
-					let locs = symbol[usage] ?? []
-					if (currentFileOnly) {
-						locs = locs.filter(l => l.uri === doc.uri)
+		try {
+			this.debug(`Getting symbol locations of usage '${searchedUsages.join(',')}' for '${doc.uri}' # ${doc.version} @ ${offset} with currentFileOnly=${currentFileOnly}`)
+			let node = AstNode.findDeepestChild({ node: file, needle: offset })
+			while (node) {
+				const symbol = this.project.symbols.resolveAlias(node.symbol)
+				if (symbol) {
+					const locations: SymbolLocation[] = []
+					for (const usage of searchedUsages) {
+						let locs = symbol[usage] ?? []
+						if (currentFileOnly) {
+							locs = locs.filter(l => l.uri === doc.uri)
+						}
+						locations.push(...locs)
 					}
-					locations.push(...locs)
+					return SymbolLocations.create(node.range, locations.length ? locations : undefined)
 				}
-				return SymbolLocations.create(node.range, locations.length ? locations : undefined)
+				node = node.parent
 			}
-			node = node.parent
+		} catch (e) {
+			this.logger.error(`[Service] [getSymbolLocations] Failed for “${doc.uri}” #${doc.version}`, e)
 		}
 		return undefined
 	}
 
 	format(node: FileNode<AstNode>, doc: TextDocument, tabSize: number, insertSpaces: boolean) {
-		this.debug(`Formatting '${doc.uri}' # ${doc.version}`)
-		const formatter = this.project.meta.getFormatter(node.type)
-		return formatter(node, FormatterContext.create(this.project, { doc, tabSize, insertSpaces }))
+		try {
+			this.debug(`Formatting '${doc.uri}' # ${doc.version}`)
+			const formatter = this.project.meta.getFormatter(node.type)
+			return formatter(node, FormatterContext.create(this.project, { doc, tabSize, insertSpaces }))
+		} catch (e) {
+			this.logger.error(`[Service] [format] Failed for “${doc.uri}” #${doc.version}`, e)
+			throw e
+		}
 	}
 }
