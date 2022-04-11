ac35adb719626f9d07a3bb4e25ec022eea6655d4
SPGoding <15277496+SPGoding@users.noreply.github.com>
2022-04-11 00:00:38 -0500

    ⚡️ Change NBT entry parser to use select

--- packages/nbt/src/parser/entry.ts
+++ packages/nbt/src/parser/entry.ts
@@ -4,11 +4,11 @@ import { byteArray, intArray, list, longArray } from './collection'
 import { compound } from './compound'
 import { primitive } from './primitive'
 
-export const entry: core.Parser<NbtNode> = (src, ctx) => core.failOnEmpty(core.any([
-	primitive,
-	compound,
-	list,
-	byteArray,
-	intArray,
-	longArray,
+export const entry: core.Parser<NbtNode> = (src, ctx) => core.failOnEmpty(core.select([
+	{ predicate: src => src.tryPeek('[B;'), parser: byteArray },
+	{ predicate: src => src.tryPeek('[I;'), parser: intArray },
+	{ predicate: src => src.tryPeek('[L;'), parser: longArray },
+	{ predicate: src => src.tryPeek('['), parser: list },
+	{ predicate: src => src.tryPeek('{'), parser: compound },
+	{ parser: primitive },
 ]))(src, ctx)
