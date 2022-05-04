c8ca38c26c7fddbbc7128c17cf5523f25dcd97e9
ChenCMD <scnmed.g@gmail.com>
2022-05-05 04:15:39 +0900

    :bug: Fix incorrect integer restrict

--- src/data/CommandTree1.19.ts
+++ src/data/CommandTree1.19.ts
@@ -2722,7 +2722,7 @@ export const CommandTree: ICommandTree = {
             parser: new LiteralArgumentParser('spawn'),
             children: {
                 pos: {
-                    parser: new VectorArgumentParser(3, 'integer')
+                    parser: new VectorArgumentParser(3)
                 }
             }
         },
@@ -2738,7 +2738,7 @@ export const CommandTree: ICommandTree = {
             parser: new LiteralArgumentParser('insert'),
             children: {
                 pos: {
-                    parser: new VectorArgumentParser(3)
+                    parser: new VectorArgumentParser(3, 'integer')
                 }
             }
         }
