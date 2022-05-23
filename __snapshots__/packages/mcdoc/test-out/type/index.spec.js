c2f7ac14879164e559484b179d3871fda6d026ef
SPGoding <15277496+SPGoding@users.noreply.github.com>
2022-05-23 17:56:51 -0500

    📸 Update snapshots

---
+++ __snapshots__/packages/mcdoc/test-out/type/index.spec.js
@@ -0,0 +1,39 @@
+exports['mcdoc checker/type.ts checkAssignability() Assign \'(string | (string | int))\' to \'(string | int)\' 1'] = {
+  "isAssignable": true
+}
+
+exports['mcdoc checker/type.ts checkAssignability() Assign \'(string | int | boolean)\' to \'(string | int)\' 1'] = {
+  "isAssignable": false,
+  "errorMessage": "Type “(string | int | boolean)” is not assignable to type “(string | int)”\n  Type “boolean” is not assignable to type “(string | int)”"
+}
+
+exports['mcdoc checker/type.ts checkAssignability() Assign \'(string | int)\' to \'(string | int | boolean)\' 1'] = {
+  "isAssignable": true
+}
+
+exports['mcdoc checker/type.ts checkAssignability() Assign \'(string | int)\' to \'string\' 1'] = {
+  "isAssignable": false,
+  "errorMessage": "Type “(string | int)” is not assignable to type “string”\n  Type “int” is not assignable to type “string”"
+}
+
+exports['mcdoc checker/type.ts checkAssignability() Assign \'[[(string | int | boolean)]]\' to \'[[(string | int)]]\' 1'] = {
+  "isAssignable": false,
+  "errorMessage": "Type “[[(string | int | boolean)]]” is not assignable to type “[[(string | int)]]”\n  Type “[(string | int | boolean)]” is not assignable to type “[(string | int)]”\n    Type “(string | int | boolean)” is not assignable to type “(string | int)”\n      Type “boolean” is not assignable to type “(string | int)”"
+}
+
+exports['mcdoc checker/type.ts checkAssignability() Assign \'[[(string | int)]]\' to \'[[(string | int | boolean)]]\' 1'] = {
+  "isAssignable": true
+}
+
+exports['mcdoc checker/type.ts checkAssignability() Assign \'int\' to \'string\' 1'] = {
+  "isAssignable": false,
+  "errorMessage": "Type “int” is not assignable to type “string”"
+}
+
+exports['mcdoc checker/type.ts checkAssignability() Assign \'string\' to \'(string | int)\' 1'] = {
+  "isAssignable": true
+}
+
+exports['mcdoc checker/type.ts checkAssignability() Assign \'string\' to \'string\' 1'] = {
+  "isAssignable": true
+}
