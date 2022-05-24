64f00eb33c740388f2c4a99b94da55cd719e4e1f
SPGoding <15277496+SPGoding@users.noreply.github.com>
2022-05-23 23:02:53 -0500

    💚 Add playground deployment

---
+++ packages/playground/deploy.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import fsp from 'fs/promises'
+import cp from 'child_process'
+import { promisify } from 'util'
+
+const execFile = promisify(cp.execFile)
+
+try {
+	const indexHtml = await fsp.readFile('./index.html')
+	const indexJs = await fsp.readFile('./dist/index.js')
+	process.chdir('../..')
+	await execFile('git', ['switch', 'gh-pages'])
+	try {
+		await fsp.mkdir('playground')
+		await fsp.mkdir('playground/dist', { recursive: true })
+	} catch (e) {
+		// Ignored
+	}
+	await fsp.writeFile('playground/index.html', indexHtml)
+	await fsp.writeFile('playground/dist/index.js', indexJs)
+	await execFile('git', ['add', 'playground'])
+	await execFile('git', ['commit', '-m "🚀 Deploy playground"'], {
+		env: {
+			GIT_COMMITTER_EMAIL: env.process.GIT_AUTHOR_EMAIL,
+			GIT_COMMITTER_NAME: env.process.GIT_AUTHOR_NAME,
+			GIT_AUTHOR_EMAIL: env.process.GIT_AUTHOR_EMAIL,
+			GIT_AUTHOR_NAME: env.process.GIT_AUTHOR_NAME,
+		},
+	})
+	await execFile('git', ['push'])
+} catch (e) {
+	console.error(e)
+	process.exitCode = 1;
+}
