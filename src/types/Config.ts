769183774a7a5c883ee0ad8cbec5e67a29f220e8
SPGoding <15277496+SPGoding@users.noreply.github.com>
2023-06-21 18:43:26 -0500

    :bug: Change default version to 1.20

--- src/types/Config.ts
+++ src/types/Config.ts
@@ -182,7 +182,7 @@ export const VanillaConfig: Config = {
         permissionLevel: 2,
         dataSource: 'GitHub',
         dataVersion: 'Latest release',
-        cmdVersion: '1.19.4',
+        cmdVersion: '1.20',
         defaultVisibility: 'public',
         dependsOnVanilla: true,
         detectionDepth: 1,
