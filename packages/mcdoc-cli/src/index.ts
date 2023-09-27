bab188a681c8f2473ad20f31c5fa1537c3044a34
Mulverine <mulverin3@gmail.com>
2023-09-26 19:03:44 -0600

    🚧 Work on mcdoc CLI

--- packages/mcdoc-cli/src/index.ts
+++ packages/mcdoc-cli/src/index.ts
@@ -1,12 +1,49 @@
 #!/usr/bin/env node
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+//import * as mcdoc from '@spyglassmc/mcdoc'
 
-await yargs(hideBin(process.argv))
-	.scriptName('mcdoc')
-	.command('generate-locale', 'Generate the default locale', () => {
-		console.log('Generating locale...')
-	})
+const CLI = yargs(hideBin(process.argv));
+
+await CLI.scriptName('mcdoc')
+	.command(
+		'generate <test>', 
+		'Generate JSON files', 
+		() => CLI.options({
+			'locale': {
+				alias: 'l',
+				description: 'en-us language key-value store of all doc comments',
+				default: false
+			},
+			'modules': {
+				alias: 'm',
+				description: 'file tree mirroring definitions; to optimize for web',
+				default: false
+			},
+			'verbose': {
+				alias: 'v',
+				default: false
+			},
+			'dry': {
+				alias: 'd',
+				description: 'will not write to disk',
+				default: false
+			}
+		}),
+		(args) => {
+			const include = [];
+
+			if (args.locale) include.push('locales');
+			if (args.modules) include.push('modules');
+
+			console.log(`Generating JSON files${args.locale || args.modules ? `, including ${include.join(', ')}` : ''}`);
+
+			const log = (key: string) => args.verbose ?? console.log(key);
+
+
+		}
+	)
 	.strict()
 	.demandCommand(1)
 	.parse()
+	
