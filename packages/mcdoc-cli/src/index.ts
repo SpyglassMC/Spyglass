c57b30e2d728ad9ad8a0dc2ce66e3c479949141f
Mulverine <mulverin3@gmail.com>
2023-10-03 22:35:20 -0600

    🧹 Run formatter

--- packages/mcdoc-cli/src/index.ts
+++ packages/mcdoc-cli/src/index.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env -S ts-node --esm
-import { dirname, join, resolve, parse } from 'path'
+import { dirname, join, parse, resolve } from 'path'
 import { fileURLToPath, pathToFileURL } from 'url'
 
 import fs from 'fs-extra'
+import walk from 'klaw'
 import lineColumn from 'line-column'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
-import walk from 'klaw'
 
-import type { AstNode } from '@spyglassmc/core';
+import type { AstNode } from '@spyglassmc/core'
 import {
 	ConfigService,
 	fileUtil,
@@ -16,68 +16,83 @@ import {
 	VanillaConfig,
 } from '@spyglassmc/core'
 import { NodeJsExternals } from '@spyglassmc/core/lib/nodejs.js'
-import * as mcdoc from '@spyglassmc/mcdoc'
 import * as je from '@spyglassmc/java-edition'
+import * as mcdoc from '@spyglassmc/mcdoc'
 
 const parentPath = dirname(fileURLToPath(import.meta.url))
 const cacheRoot = join(parentPath, 'cache')
 
-const CLI = yargs(hideBin(process.argv));
+const CLI = yargs(hideBin(process.argv))
 
 await CLI.scriptName('mcdoc')
 	.command(
-		'generate [source]', 
-		'Generate JSON files', 
-		() => CLI.positional(
-			'source', {
-				describe: 'path to directory containing mcdoc source',
-				type: 'string',
-				default: '.'
-			}
-		).options({
-			'locale': {
-				alias: 'l',
-				description: 'en-us language key-value store of all doc comments',
-				default: false
-			},
-			'module': {
-				alias: 'm',
-				description: 'file tree mirroring definitions; to optimize for web',
-				default: false
-			},
-			'pretty': {
-				alias: 'p',
-				description: 'pretty printed variants',
-				default: false
-			},
-			'verbose': {
-				alias: 'v',
-				default: false
-			},
-			'dry': {
-				alias: 'd',
-				description: 'will not write to disk',
-				default: false
-			}
-		}).boolean('locale').boolean('module').boolean('pretty').boolean('verbose').boolean('dry'),
+		'generate [source]',
+		'Generate JSON files',
+		() =>
+			CLI.positional(
+				'source',
+				{
+					describe: 'path to directory containing mcdoc source',
+					type: 'string',
+					default: '.',
+				},
+			).options({
+				'locale': {
+					alias: 'l',
+					description:
+						'en-us language key-value store of all doc comments',
+					default: false,
+				},
+				'module': {
+					alias: 'm',
+					description:
+						'file tree mirroring definitions; to optimize for web',
+					default: false,
+				},
+				'pretty': {
+					alias: 'p',
+					description: 'pretty printed variants',
+					default: false,
+				},
+				'verbose': {
+					alias: 'v',
+					default: false,
+				},
+				'dry': {
+					alias: 'd',
+					description: 'will not write to disk',
+					default: false,
+				},
+			}).boolean('locale').boolean('module').boolean('pretty').boolean(
+				'verbose',
+			).boolean('dry'),
 		async (args) => {
-			const include = [];
+			const include = []
 
-			if (args.locale) include.push('locales');
-			if (args.module) include.push('modules');
+			if (args.locale) include.push('locales')
+			if (args.module) include.push('modules')
 
-			console.info(`Generating JSON files${args.locale || args.module ? `, including ${include.join(', ')}` : ''}`);
+			console.info(
+				`Generating JSON files${
+					args.locale || args.module
+						? `, including ${include.join(', ')}`
+						: ''
+				}`,
+			)
 
 			const logger = {
-				log: (...log_args: any[]) => args.verbose ? console.log(...log_args) : false,
+				log: (...log_args: any[]) =>
+					args.verbose ? console.log(...log_args) : false,
 
 				warn: (...log_args: any[]) => console.warn(...log_args),
 
 				error: (...log_args: any[]) => console.error(...log_args),
 
-				info: (...log_args: any[]) => args.verbose ? console.info(...log_args) : false,
+				info: (...log_args: any[]) =>
+					args.verbose ? console.info(...log_args) : false,
 
-				trace: (message?: any, ...params: any) => console.trace(message, ...params)
+				trace: (message?: any, ...params: any) =>
+					console.trace(message, ...params),
 			}
 
 			const projectPath = resolve(parentPath, args.source)
@@ -98,139 +113,192 @@ await CLI.scriptName('mcdoc')
 						pathToFileURL(projectPath).toString(),
 					),
 				},
-			});
+			})
 
-			await service.project.ready();
-			await service.project.cacheService.save();
+			await service.project.ready()
+			await service.project.cacheService.save()
 
-			const out = 'out';
+			const out = 'out'
 
 			if (args.dry !== true) {
 				await fs.ensureDir(out)
 			}
 
-			const symbols = [];
+			const symbols = []
 
-			const internal_locales: Record<string, string[]> = {};
+			const internal_locales: Record<string, string[]> = {}
 
-			const locales: Record<string, string> = {};
+			const locales: Record<string, string> = {}
 
-			let errors = 0;
-			
+			let errors = 0
+
 			for await (const doc_file of walk(projectPath)) {
 				if (doc_file.path.endsWith('.mcdoc')) {
-					const DocumentUri = pathToFileURL(doc_file.path).toString();
+					const DocumentUri = pathToFileURL(doc_file.path).toString()
 
-					const doc_contents = await fs.readFile(doc_file.path, 'utf-8');
+					const doc_contents = await fs.readFile(doc_file.path, 'utf-8')
 
-					await service.project.onDidOpen(DocumentUri, 'mcdoc', 0, doc_contents);
+					await service.project.onDidOpen(
+						DocumentUri,
+						'mcdoc',
+						0,
+						doc_contents,
+					)
 
-					const check = await service.project.ensureClientManagedChecked(DocumentUri);
+					const check = await service.project.ensureClientManagedChecked(
+						DocumentUri,
+					)
 
 					if (check && check.doc && check.node) {
-						const { doc, node } = check;
+						const { doc, node } = check
 
-						const path = parse(fileURLToPath(doc.uri));
+						const path = parse(fileURLToPath(doc.uri))
 
-						const resource = join(path.dir.replace(`${projectPath}`, ''), path.name).replace(/^\//, '');
+						const resource = join(
+							path.dir.replace(`${projectPath}`, ''),
+							path.name,
+						).replace(/^\//, '')
 
 						logger.info(`parsing ${resource}\n`)
 
 						if (node.children[0]) {
 							const children = node.children
 
-							function flattenChild (parent: string, self: string, _parent: Partial<AstNode> | undefined, _child: Partial<AstNode>) {
-								const child: any = {};
+							function flattenChild(
+								parent: string,
+								self: string,
+								_parent: Partial<AstNode> | undefined,
+								_child: Partial<AstNode>,
+							) {
+								const child: any = {}
 
-								let known_error = false;
+								let known_error = false
 
 								/* @ts-ignore */
-								child.self = self;
+								child.self = self
 
 								/* @ts-ignore */
-								if (_child.parent) child.parent = parent;
+								if (_child.parent) child.parent = parent
 
 								/* @ts-ignore */
-								if (_child.parentMap) child.parentMap = parent;
+								if (_child.parentMap) child.parentMap = parent
 
-								
 								if (_child.children) {
-									child.children = [];
-									for (const [i, __child] of Object.entries(_child.children)) {
+									child.children = []
+									for (
+										const [i, __child] of Object.entries(
+											_child.children,
+										)
+									) {
 										/* @ts-ignore */
-										child.children[Number(i)] = flattenChild(self, `${self}[${i}]`, _child, __child)
+										child.children[Number(i)] = flattenChild(
+											self,
+											`${self}[${i}]`,
+											_child,
+											__child,
+										)
 									}
 								}
 
-								child.type = _child.type;
+								child.type = _child.type
 
 								/* @ts-ignore */
-								if (Object.hasOwn(_child, 'isOptional')) child.isOptional = _child.isOptional;
+								if (Object.hasOwn(_child, 'isOptional')) {
+									child.isOptional = _child.isOptional
+								}
 
 								/* @ts-ignore */
-								if (Object.hasOwn(_child, 'colorTokenType')) child.colorTokenType = _child.colorTokenType;
+								if (Object.hasOwn(_child, 'colorTokenType')) {
+									child.colorTokenType = _child.colorTokenType
+								}
 
 								/* @ts-ignore */
 								if (Object.hasOwn(_child, 'value')) {
 									/* @ts-ignore */
-									child.value = _child.value;
+									child.value = _child.value
 
 									if (internal_locales[parent]) {
-										locales[`mcdoc.${resource.replace(/\//g, '.')}.${child.value}`] = internal_locales[parent].join('\n');
+										locales[
+											`mcdoc.${
+												resource.replace(/\//g, '.')
+											}.${child.value}`
+										] = internal_locales[parent].join('\n')
 
-										delete internal_locales[parent];
+										delete internal_locales[parent]
 									}
 								}
 
-								if (child.type === 'mcdoc:struct/map_key' && internal_locales[parent]) {
-									locales[`mcdoc.${resource.replace(/\//g, '.')}.map_key`] = internal_locales[parent].join('\n');
+								if (
+									child.type === 'mcdoc:struct/map_key' &&
+									internal_locales[parent]
+								) {
+									locales[
+										`mcdoc.${resource.replace(/\//g, '.')}.map_key`
+									] = internal_locales[parent].join('\n')
 
-									delete internal_locales[parent];
+									delete internal_locales[parent]
 								}
 
 								if (Object.hasOwn(_child, 'comment')) {
 									/* @ts-ignore */
-									const comment: string = _child.comment.trim();
-									child.comment = comment;
+									const comment: string = _child.comment.trim()
+									child.comment = comment
 
-									if (!args.dry && args.locale && _parent?.type === 'mcdoc:doc_comments') {
-										const key = parent.replace(/\[\d+\]$/, '');
+									if (
+										!args.dry && args.locale &&
+										_parent?.type === 'mcdoc:doc_comments'
+									) {
+										const key = parent.replace(/\[\d+\]$/, '')
 
-										if (!internal_locales.key) internal_locales[key] = [];
+										if (!internal_locales.key) {
+											internal_locales[key] = []
+										}
 
-										internal_locales[key].push(comment);
+										internal_locales[key].push(comment)
 									} else if (comment.startsWith('/ ')) {
-										child.type = 'error';
-										console.warn(`known error: orphaned dispatch comment`);
-										known_error = true;
+										child.type = 'error'
+										console.warn(
+											`known error: orphaned dispatch comment`,
+										)
+										known_error = true
 									}
 								}
 
-								if (_child.hover) child.hover = _child.hover;
+								if (_child.hover) child.hover = _child.hover
 
-								if (_child.color) child.color = _child.color;
+								if (_child.color) child.color = _child.color
 
-								if (child.type !== 'error') return child;
+								if (child.type !== 'error') return child
 								else {
-									errors++;
+									errors++
 
 									const lc = lineColumn(doc_contents)
 
-									function range(range: {start: number, end: number}) {
-										const start = lc.fromIndex(range.start);
-										const end = lc.fromIndex(range.end);
+									function range(
+										range: { start: number; end: number },
+									) {
+										const start = lc.fromIndex(range.start)
+										const end = lc.fromIndex(range.end)
 
-										return `L${start?.line}${start?.col ? `:C${start?.col}` : ''} -> L${end?.line}${end?.col ? `:C${end?.col}` : ''}`;
+										return `L${start?.line}${
+											start?.col ? `:C${start?.col}` : ''
+										} -> L${end?.line}${
+											end?.col ? `:C${end?.col}` : ''
+										}`
 									}
 
 									if (!known_error) {
-										console.warn(`mcdoc error(s):`);
+										console.warn(`mcdoc error(s):`)
 										/* @ts-ignore */
 										if (_child.parent?.parserErrors.length !== 0) {
 											console.warn('	parser:')
 											/* @ts-ignore */
 											_child.parent?.parserErrors.forEach(error => {
-												console.warn(`		${error.message}\n		Location: ${range(error.range)}. Severity ${error.severity}.`)
+												console.warn(
+													`		${error.message}\n		Location: ${
+														range(error.range)
+													}. Severity ${error.severity}.`,
+												)
 											})
 										}
 
@@ -239,37 +307,56 @@ await CLI.scriptName('mcdoc')
 											console.warn('	binder:')
 											/* @ts-ignore */
 											_child.parent?.binderErrors.forEach(error => {
-												console.warn(`		${error.message}\n		Location: ${range(error.range)}. Severity ${error.severity}.`)
+												console.warn(
+													`		${error.message}\n		Location: ${
+														range(error.range)
+													}. Severity ${error.severity}.`,
+												)
 											})
 										}
 									}
-									console.warn(`error @ ${doc_file.path}\n\n`);
-									return false;
+									console.warn(`error @ ${doc_file.path}\n\n`)
+									return false
 								}
 							}
 
 							children.forEach((child, i) => {
 								/* @ts-ignore */
-								children[i] = flattenChild(resource, `${resource}.[${i}]`, undefined, child);
+								children[i] = flattenChild(
+									resource,
+									`${resource}.[${i}]`,
+									undefined,
+									child,
+								)
 							})
 
 							const symbol = {
 								resource,
-	
-								children,
-							};
 
-							symbols.push(symbol);
+								children,
+							}
+
+							symbols.push(symbol)
 
 							if (!args.dry && args.module) {
-								const dir = parse(join(out, 'module', resource)).dir;
-								
-								if (dir !== '') await fs.ensureDir(dir);
+								const dir = parse(join(out, 'module', resource)).dir
 
-								await fs.writeFile(join(out, 'module', `${resource}.mcdoc.json`), JSON.stringify(symbol));
+								if (dir !== '') await fs.ensureDir(dir)
+
+								await fs.writeFile(
+									join(out, 'module', `${resource}.mcdoc.json`),
+									JSON.stringify(symbol),
+								)
 
 								if (args.pretty) {
-									await fs.writeFile(join(out, 'module', `${resource}.pretty.mcdoc.json`), JSON.stringify(symbol, undefined, 3));
+									await fs.writeFile(
+										join(
+											out,
+											'module',
+											`${resource}.pretty.mcdoc.json`,
+										),
+										JSON.stringify(symbol, undefined, 3),
+									)
 								}
 							}
 						}
@@ -278,32 +365,45 @@ await CLI.scriptName('mcdoc')
 			}
 
 			if (!args.dry) {
-				await fs.writeFile(join(out, 'generated.mcdoc.json'), JSON.stringify(symbols));
+				await fs.writeFile(
+					join(out, 'generated.mcdoc.json'),
+					JSON.stringify(symbols),
+				)
 
 				if (args.pretty) {
-					await fs.writeFile(join(out, 'generated.pretty.mcdoc.json'), JSON.stringify(symbols, undefined, 3));
+					await fs.writeFile(
+						join(out, 'generated.pretty.mcdoc.json'),
+						JSON.stringify(symbols, undefined, 3),
+					)
 				}
 
 				if (args.module) {
-					await fs.writeFile(join(out, 'module', 'index.json'), JSON.stringify(symbols.map(symbol => symbol.resource)));
+					await fs.writeFile(
+						join(out, 'module', 'index.json'),
+						JSON.stringify(symbols.map(symbol => symbol.resource)),
+					)
 				}
 
 				if (args.locale) {
-					const orphaned_doc = Object.keys(internal_locales);
+					const orphaned_doc = Object.keys(internal_locales)
 					if (orphaned_doc.length !== 0) {
-						console.warn(`parsing error, ${orphaned_doc.length} orphaned doc comments or incorrectly parsed markup comments`);
-						console.warn(internal_locales);
+						console.warn(
+							`parsing error, ${orphaned_doc.length} orphaned doc comments or incorrectly parsed markup comments`,
+						)
+						console.warn(internal_locales)
 					}
-					await fs.writeFile(join(out, 'locale.en-us.json'), JSON.stringify(locales, undefined, 3))
+					await fs.writeFile(
+						join(out, 'locale.en-us.json'),
+						JSON.stringify(locales, undefined, 3),
+					)
 				}
 			}
 
 			console.log(`Generated JSON files with ${errors} errors.`)
 
 			await service.project.close()
-		}
+		},
 	)
 	.strict()
 	.demandCommand(1)
 	.parse()
-	
