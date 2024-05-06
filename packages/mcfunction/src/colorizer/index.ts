cd8d097234844a0808496e972c3c8f2cc052d7c1
Misode <Misoloo64@gmail.com>
2024-05-06 03:29:58 +0200

    ✨ Add simple macro parser and colorizer (#1117)

--- packages/mcfunction/src/colorizer/index.ts
+++ packages/mcfunction/src/colorizer/index.ts
@@ -1,10 +1,15 @@
 import * as core from '@spyglassmc/core'
 import type {
+	CommandMacroNode,
 	LiteralCommandChildNode,
 	TrailingCommandChildNode,
 } from '../node/index.js'
 
 export function register(meta: core.MetaRegistry) {
+	meta.registerColorizer<CommandMacroNode>(
+		'mcfunction:command_macro',
+		macro,
+	)
 	meta.registerColorizer<LiteralCommandChildNode>(
 		'mcfunction:command_child/literal',
 		core.colorizer.literal,
@@ -13,4 +18,8 @@ export function register(meta: core.MetaRegistry) {
 		'mcfunction:command_child/trailing',
 		core.colorizer.error,
 	)
+}
+
+export const macro: core.Colorizer<CommandMacroNode> = (node, ctx) => {
+	return [core.ColorToken.create(node, 'string')]
 }
