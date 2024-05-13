c4a6f25ca5226fd7542ce9c19d6b4f44e4970a8d
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-05-13 12:35:43 -0400

    ✅ add explicit unit test for JSON array parser error absorbing

--- packages/json/test/parser/array.spec.ts
+++ packages/json/test/parser/array.spec.ts
@@ -1,7 +1,9 @@
+import type { LanguageError } from '@spyglassmc/core'
 import {
 	showWhitespaceGlyph,
 	testParser,
 } from '@spyglassmc/core/test-out/utils.js'
+import assert from 'assert'
 import snapshot from 'snap-shot-it'
 import { array } from '../../lib/parser/array.js'
 
@@ -24,5 +26,48 @@ describe('JSON array parser', () => {
 				snapshot(testParser(array, content))
 			})
 		}
+
+		describe('should absorb and output parse errors from child parsers', () => {
+			const cases: {
+				name: string
+				content: string
+				expectedErrors: LanguageError[]
+			}[] = [
+				{
+					name: 'invalid character escape',
+					content: '["\\z"]',
+					expectedErrors: [
+						{
+							range: {
+								start: 3,
+								end: 4,
+							},
+							message: 'Unexpected escape character “z”',
+							severity: 3,
+						},
+					],
+				},
+				{
+					name: 'invalid unicode escape',
+					content: '["\\u1z34"]',
+					expectedErrors: [
+						{
+							range: {
+								start: 4,
+								end: 8,
+							},
+							message: 'Hexadecimal digit expected',
+							severity: 3,
+						},
+					],
+				},
+			]
+			for (const { name, content, expectedErrors } of cases) {
+				it(name, () => {
+					const { errors } = testParser(array, content)
+					assert.deepEqual(errors, expectedErrors)
+				})
+			}
+		})
 	})
 })
