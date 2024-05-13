82eaccb245a70d978e518b75719e4ed546440928
Misode <Misoloo64@gmail.com>
2024-05-13 21:14:46 +0200

    🔥 Remove old json checkers (#1127)

--- packages/java-edition/src/json/checker/util/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './advancement.js'
-export * from './block_states.js'
-export * from './color.js'
-export * from './nbt.js'
-export * from './recipe.js'
-export * from './uuid.js'
-export * from './version.js'
--- packages/json/src/checker/JsonChecker.ts
+++
@@ -1,9 +0,0 @@
-import type { CheckerContext } from '@spyglassmc/core'
-import type { JsonNode } from '../node/index.js'
-
-export interface JsonCheckerContext extends CheckerContext {
-	context: string
-	depth?: number
-}
-
-export type JsonChecker = (node: JsonNode, ctx: JsonCheckerContext) => void
