f1d305b64d72cab105f1de0b3b50a128335e06af
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-05-13 12:22:23 -0400

    ✅ add unit test for JSON array parser - covers generic cases, not the bug yet

---
+++ packages/json/test/parser/array.spec.ts
@@ -0,0 +1,26 @@
+import {
+	showWhitespaceGlyph,
+	testParser,
+} from '@spyglassmc/core/test-out/utils.js'
+import snapshot from 'snap-shot-it'
+import { array } from '../../lib/parser/array.js'
+
+describe('JSON array parser', () => {
+	describe('array()', () => {
+		const cases: { content: string }[] = [
+			{ content: '' },
+			{ content: '[]' },
+			{ content: '[1]' },
+			{ content: '[1,2]' },
+			{ content: '[1,2,]' },
+			{ content: '[[1],2]' },
+			{ content: '["hey","there"]' },
+			{ content: '["\\"","\\u1234"]' },
+		]
+		for (const { content } of cases) {
+			it(`Parse "${showWhitespaceGlyph(content)}"`, () => {
+				snapshot(testParser(array, content))
+			})
+		}
+	})
+})
