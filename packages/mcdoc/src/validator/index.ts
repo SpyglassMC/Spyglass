45a53812e37588f239b6cc2149aa1fc6192d2e9e
NeunEinser <daniel30797@gmail.com>
2024-05-19 23:48:46 +0200

    Add mcdoc simplify method

--- packages/mcdoc/src/validator/index.ts
+++ packages/mcdoc/src/validator/index.ts
@@ -1,7 +1,7 @@
-import type { AstNode, CheckerContext } from "@spyglassmc/core"
-import type { Attribute, ListType, LiteralNumericValue, LiteralValue, NumericType, PrimitiveArrayType, StringType, StructType, TupleType} from "../type";
-import { NumericRange, type LiteralType, type McdocType, type StructTypePairField } from "../type"
-import { TypeDefSymbolData } from "../binder"
+import type { AstNode, CheckerContext, Range } from "@spyglassmc/core"
+import type { Attribute, StructTypePairField , LiteralType, McdocType, StructTypeField } from "../type/index.js";
+import { NumericRange } from "../type/index.js"
+import { TypeDefSymbolData } from "../binder/index.js"
 import { arrayToMessage, localize } from "@spyglassmc/locales";
 
 export type McdocTypeInferrer<T extends AstNode> = (node: T) => McdocType
@@ -111,44 +111,230 @@ function internalTypeDefinition<T extends AstNode>(node: T, typeDef: McdocType, 
 		options.attachTypeInfo(node, typeDef);
 		return [];
 	}
+
+	typeDef = simplify(node, typeDef, inferredType, options, parents);
+
+	const inferredValueType = getValueType(inferredType);
+	const expectedValueType = getValueType(typeDef);
+
+	if (inferredValueType.kind !== expectedValueType.kind && !options.isEquivalent(inferredValueType, expectedValueType)) {
+		return [{
+			kind: "type_mismatch",
+			node: node,
+			received: inferredType,
+			expected: [typeDef]
+		}];
+	}
 	
+	options.attachTypeInfo(node, typeDef);
+
 	switch (typeDef.kind) {
 		case 'any':
 		case 'unsafe':
 			options.attachTypeInfo(node, typeDef);
 			return [];
-		case 'reference':
-			if (!typeDef.path) {
-				// TODO when does this happen?
-				return [];
-			}
-			return internalReference(node, typeDef.path, options, parents)
 		case 'attributed':
 			const attribute = attributeHandlers[typeDef.attribute.name];
 			let attributeErrors: ValidationError<T>[] = []
 			if (attribute) {
 				attributeErrors = attribute(node, typeDef.attribute, inferredType, typeDef, options);
 			}
-			// TODO apply attrubute function? The child node needs to be aware of some attributes.
-			Array.prototype.push.apply(attributeErrors, internalTypeDefinition(node, typeDef.child, inferredType, options, parents));
+			// TODO pass attribute to given node? The node needs to be aware of some attributes for colorizers and completers.
+			attributeErrors.push(...internalTypeDefinition(node, typeDef.child, inferredType, options, parents));
 			return attributeErrors;
+		case 'union':
+			if (typeDef.members.length === 0) {
+				return [{
+					kind: 'expected_noting',
+					node: node,
+				}];
+			}
+
+			let parsingResults: { typeDef: McdocType, errors: ValidationError<T>[] }[] = [];
+			for (const def of typeDef.members) {
+				// TODO mock attachTypeInfo method
+				const innerErrors = internalTypeDefinition(node, def, inferredType, options, parents);
+				if (innerErrors.length === 0) {
+					return [];
+				}
+				parsingResults.push({ typeDef: def, errors: innerErrors });
+			}
+			if (!parsingResults.some(r => r.errors.some(e => e.kind !== 'type_mismatch' || e.node !== node))) {
+				return [{
+					kind: 'type_mismatch',
+					node: node,
+					expected: typeDef.members,
+					received: inferredType
+				}];
+			}
+			if (!parsingResults.some(r => r.errors.some(e => e.kind !== 'value_mismatch' || e.node !== node))) {
+				return [{
+					kind: 'value_mismatch',
+					node: node,
+					expected: parsingResults.flatMap(p => p.errors).filter(e => e.kind === 'value_mismatch').flatMap(e => (e as ValueMismatchError<T>).expected),
+				}];
+			}
+			parsingResults = parsingResults.filter(r => !r.errors.some(e => e.kind === 'type_mismatch' && e.node === node));
+			if (!parsingResults.some(r => r.errors.some(e => e.kind === 'unknown_key'))) {
+				parsingResults = parsingResults.filter(r => !r.errors.some(e => e.kind === 'unknown_key'));
+			}
+			return parsingResults.sort((a, b) => a.errors.length - b.errors.length)[0].errors;
+		case 'struct': {
+			const children = options.getChildren(node);
+			const matchedToNonLiteral = [];
+
+			// TODO
+			for (const pair of typeDef.fields.reverse()) {
+				if (pair.kind === 'pair') {
+					
+				}
+			}
+			break;
+		}
+		case 'list':
+		case 'byte_array':
+		case 'int_array':
+		case 'long_array': {
+			let itemType: McdocType;
+			switch (typeDef.kind) {
+				case 'list':
+					itemType = typeDef.item;
+					break;
+				case 'byte_array':
+					itemType = { kind: 'byte', valueRange: typeDef.valueRange };
+					break;
+				case 'int_array':
+					itemType = { kind: 'int', valueRange: typeDef.valueRange };
+					break;
+				case 'long_array': 
+					itemType = { kind: 'long', valueRange: typeDef.valueRange };
+					break;
+			}
+			const children = options.getChildren(node);
+			for (const child of children) {
+				const inferredChild = options.inferType(child);
+				errors.push(...internalTypeDefinition(child, itemType, inferredChild, options, [ ...parents, node ]));
+			}
+			if (typeDef.lengthRange && !NumericRange.isInRange(typeDef.lengthRange, children.length)) {
+				errors.push({
+					kind: 'invalid_collection_length',
+					node: node,
+					range: typeDef.lengthRange,
+				})
+			}
+			break;
+		}
+		case 'tuple': {
+			const children = options.getChildren(node);
+			if (typeDef.items.length === children.length) {
+				for (let i = 0; i < typeDef.items.length; i++) {
+					const def = typeDef.items[i];
+					const child = children[i];
+					const inferredChild = options.inferType(child);
+					errors.push(...internalTypeDefinition(child, def, inferredChild, options, [ ...parents, node ]));
+				}
+			} else {
+				errors.push({
+					kind: 'invalid_collection_length',
+					node: node,
+					range: { kind: 0b00, max: typeDef.items.length, min: typeDef.items.length },
+				})
+			}
+			break;
+		}
+		case 'literal': {
+			if (inferredType.kind !== 'literal' || typeDef.value.value !== inferredType.value.value) {
+				return [{
+					kind: "value_mismatch",
+					node: node,
+					expected: [typeDef.value.value]
+				}];
+			}
+			break;
+		}
+		case 'enum': {
+			if (inferredType.kind === 'literal' && !typeDef.values.some(v => {
+				if (v.attributes) {
+					const enumChildType = {
+						kind: 'literal',
+						value: { kind: typeDef.enumKind ?? 'int', value: v.value}
+					} as LiteralType
+					const attributeErrors = v.attributes.flatMap(a => {
+						const h = attributeHandlers[a.name]
+						return h ? h(node, a, inferredType, enumChildType, options) : []
+					})
+					if (attributeErrors.length > 0) {
+						return false;
+					}
+				}
+				return v.value === inferredType.value.value;
+			})) {
+				return [{
+					kind: "value_mismatch",
+					node: node,
+					expected: typeDef.values.map(v => v.value)
+				}];
+			}
+		}
+	}
+	return errors;
+}
+
+function simplify<T extends AstNode>(node: T, typeDef: McdocType, inferredType: McdocType, options: ValidatorOptions<T>, parents:T[]): McdocType {
+	switch (typeDef.kind) {
+		case 'reference':
+			if (!typeDef.path) {
+				// TODO when does this happen?
+				options.context.logger.warn(`Tried to access empty reference`);
+				return { kind: 'union', members: [] };
+			}
+			const def = options.context.symbols.query(options.context.doc, 'mcdoc', typeDef.path).getData(TypeDefSymbolData.is)?.typeDef;
+			if (!def) {
+				options.context.logger.warn(`Tried to access unknown reference ${typeDef.path}`);
+				return { kind: 'union', members: [] };
+			}
+
+			return simplify(node, def, inferredType, options, parents);
+		case 'attributed':
+			// TODO Basic attribute checks (mostly until/since).
+			return { ...typeDef, child: simplify(node, typeDef.child, inferredType, options, parents) };
 		case 'dispatcher':
 			const dispatcher = options.context.symbols.query(options.context.doc, 'mcdoc/dispatcher', typeDef.registry).symbol?.members;
 			if (!dispatcher) {
 				options.context.logger.warn(`Tried to access unknown dispatcher ${typeDef.registry}`);
-				return [];
+				return { kind: 'union', members: [] };
 			}
-			let dispatcherValues: McdocType[] = [];
-		
+			const structFields: StructTypePairField[] = [];
+			for (const key in dispatcher) {
+
+				// TODO Better way to access typedef?
+				const data = dispatcher[key].data as any;
+				if (data && data.typeDef) {
+					structFields.push({ kind: 'pair', key: key, type: data.typeDef });
+				}
+			}
+			return simplify(node, { kind: 'indexed', parallelIndices: typeDef.parallelIndices, child: { kind: 'struct', fields: structFields } }, inferredType, options, parents);
+		case 'indexed':
+			const child = simplify(node, typeDef.child, inferredType, options, parents);
+			if (child.kind ==='attributed') {
+				const ans = simplify(node, { ...typeDef, child: child.child }, inferredType, options, parents);
+				return { ...child, child: ans };
+			}
+
+			if (child.kind !== 'struct') {
+				options.context.logger.warn(`Tried to index unindexable type ${child.kind}`);
+				return { kind: 'union', members: [] };
+			}
+			let values: McdocType[] = [];
+
 			for (const index of typeDef.parallelIndices) {
-				let dispatcherLookup: string | undefined;
+				let lookup: string | undefined;
 				if (index.kind === 'static') {
 					if (index.value === '%fallback') {
-						// TODO how to properly check e.data.typeDef is a thing and a McdocType
-						dispatcherValues = Object.values(dispatcher).map(e => (e.data as any)?.typeDef).filter(e => e);
+						values = child.fields.filter(f => f.kind === 'pair').map(f => f.type);
 						break;
 					}
-					dispatcherLookup = index.value;
+					lookup = index.value;
 				} else {
 					const currentParents = [ ...parents ];
 					let current = currentParents.pop();
@@ -185,201 +371,84 @@ function internalTypeDefinition<T extends AstNode>(node: T, typeDef: McdocType, 
 					if (current) {
 						const typeOfFoundValue = options.inferType(current);
 						if (typeOfFoundValue.kind === 'literal' && typeOfFoundValue.value.kind === 'string') {
-							dispatcherLookup = typeOfFoundValue.value.value;
-							if (dispatcherLookup.startsWith('minecraft:')) {
-								dispatcherLookup = dispatcherLookup.substring(10);
+							lookup = typeOfFoundValue.value.value;
+							if (lookup.startsWith('minecraft:')) {
+								lookup = lookup.substring(10);
 							}
 						}
 					}
 				}
 
-				const dispatcherValue = dispatcher[dispatcherLookup ?? '%unknown']
-				// TODO how to properly check e.data.typeDef is a thing and a McdocType
-				if ((dispatcherValue?.data as any)?.typeDef) {
-					dispatcherValues.push((dispatcherValue?.data as any)?.typeDef)
+				lookup ??= '%none'
+				const value = child.fields.find(f => f.kind === 'pair' && f.key === lookup)
+				if (value) {
+					values.push(value.type)
+				} else {
+					values = child.fields.filter(f => f.kind === 'pair').map(f => f.type);
+					break;
 				}
 			}
-			return internalTypeDefinition(node, { kind: 'union', members: dispatcherValues }, inferredType, options, parents);
-		case 'indexed':
-			// TODO
-			break;
+			return simplify(node, { kind: 'union', members: values }, inferredType, options, parents);
 		case 'union':
-			if (typeDef.members.length === 0) {
-				return [{
-					kind: 'expected_noting',
-					node: node,
-				}];
-			}
-
-			let parsingResults: { typeDef: McdocType, errors: ValidationError<T>[] }[] = [];
-			for (const def of typeDef.members) {
-				const innerErrors = internalTypeDefinition(node, def, inferredType, options, parents);
-				if (innerErrors.length === 0) {
-					return [];
+			const members: McdocType[] = [];
+			for (const member of typeDef.members) {
+				const simplified = simplify(node, member, inferredType, options, parents);
+				const attributes: Attribute[] = [];
+				let inner = simplified;
+				// TODO figure out if we can maybe handle attributes in simplify already, so after simplify there is no attributed types
+				// Maybe it would also be worth just attaching an attributes array to type nodes and getting rid of the attributed type alltogether
+				while (inner.kind === 'attributed') {
+					attributes.push(inner.attribute);
+					inner = inner.child;
 				}
-				parsingResults.push({ typeDef: def, errors: innerErrors });
-			}
-			if (!parsingResults.some(r => r.errors.some(e => e.kind !== 'type_mismatch' || e.node !== node))) {
-				return [{
-					kind: 'type_mismatch',
-					node: node,
-					expected: typeDef.members,
-					received: inferredType
-				}];
-			}
-			if (!parsingResults.some(r => r.errors.some(e => e.kind !== 'value_mismatch' || e.node !== node))) {
-				return [{
-					kind: 'value_mismatch',
-					node: node,
-					expected: parsingResults.flatMap(p => p.errors).filter(e => e.kind === 'value_mismatch').flatMap(e => (e as ValueMismatchError<T>).expected),
-				}];
-			}
-			parsingResults = parsingResults.filter(r => !r.errors.some(e => e.kind === 'type_mismatch' && e.node === node));
-			if (!parsingResults.some(r => r.errors.some(e => e.kind === 'unknown_key'))) {
-				parsingResults = parsingResults.filter(r => !r.errors.some(e => e.kind === 'unknown_key'));
-			}
-			return parsingResults.sort((a, b) => a.errors.length - b.errors.length)[0].errors;
-		case 'concrete':
-			// TODO
-			break;
-		case 'template':
-			// TODO
-			break;
-	}
-
-	const inferredValueType = getValueType(inferredType);
-	const expectedValueType = getValueType(typeDef);
-
-	if (inferredValueType.kind !== expectedValueType.kind && !options.isEquivalent(inferredValueType, expectedValueType)) {
-		return [{
-			kind: "type_mismatch",
-			node: node,
-			received: inferredType,
-			expected: [typeDef]
-		}];
-	}
-	
-	options.attachTypeInfo(node, typeDef);
-
-	switch (typeDef.kind) {
-		case 'struct': {
-			// TODO handle spread types
-			// TODO check duplicate fields
-			// TODO check missing fields
-			const defChildren = typeDef.fields.filter(f => f.kind === 'pair') as StructTypePairField[];
-
-			for (const child of options.getChildren(node)) {
-				const kvp = options.getChildren(child);
-				
-				if (kvp.length !== 2) {
-					errors.push({ kind: 'expected_key_value_pair', node: child });
-					continue;
+				if (inner.kind === 'union') {
+					for (let innerMember of inner.members) {
+						for (const attribute of attributes.reverse()) {
+							innerMember = { kind: 'attributed', attribute: attribute, child: innerMember };
+						}
+						members.push(innerMember);
+					}
+				} else {
+					members.push(simplified);
 				}
-
-				const inferredKey = options.inferType(kvp[0]);
-				const childDefKey = findStructKey(kvp[0], defChildren.map(c => c.key), inferredKey, options, [ ...parents, node ]);
-				
-				if (childDefKey === undefined) {
-					errors.push({
-						kind: 'unknown_key', node: kvp[0],
-						key: inferredKey.kind === 'literal' ? inferredKey.value.value.toString() : undefined,
-					});
-					continue;
-				}
-
-				const inferredChild = options.inferType(kvp[1]);
-				const childDef = defChildren.find(d => d.key === childDefKey);
-				Array.prototype.push.apply(errors, internalTypeDefinition(kvp[1], childDef!.type, inferredChild, options, [ ...parents, node ]));
 			}
-			break;
-		}
-		case 'list':
-		case 'byte_array':
-		case 'int_array':
-		case 'long_array': {
-			let itemType: McdocType;
-			switch (typeDef.kind) {
-				case 'list':
-					itemType = typeDef.item;
-					break;
-				case 'byte_array':
-					itemType = { kind: 'byte', valueRange: typeDef.valueRange };
-					break;
-				case 'int_array':
-					itemType = { kind: 'int', valueRange: typeDef.valueRange };
-					break;
-				case 'long_array': 
-					itemType = { kind: 'long', valueRange: typeDef.valueRange };
-					break;
+			if (members.length === 1) {
+				return members[0];
 			}
-			const children = options.getChildren(node);
-			for (const child of children) {
-				const inferredChild = options.inferType(child);
-				Array.prototype.push.apply(errors, internalTypeDefinition(child, itemType, inferredChild, options, [ ...parents, node ]));
-			}
-			if (typeDef.lengthRange && !NumericRange.isInRange(typeDef.lengthRange, children.length)) {
-				errors.push({
-					kind: 'invalid_collection_length',
-					node: node,
-					range: typeDef.lengthRange,
-				})
-			}
-			break;
-		}
-		case 'tuple': {
-			const children = options.getChildren(node);
-			if (typeDef.items.length === children.length) {
-				for (let i = 0; i < typeDef.items.length; i++) {
-					const def = typeDef.items[i];
-					const child = children[i];
-					const inferredChild = options.inferType(child);
-					Array.prototype.push.apply(errors, internalTypeDefinition(child, def, inferredChild, options, [ ...parents, node ]));
-				}
-			} else {
-				errors.push({
-					kind: 'invalid_collection_length',
-					node: node,
-					range: { kind: 0b00, max: typeDef.items.length, min: typeDef.items.length },
-				})
-			}
-			break;
-		}
-		case 'literal': {
-			if (inferredType.kind !== 'literal' || typeDef.value.value !== inferredType.value.value) {
-				return [{
-					kind: "value_mismatch",
-					node: node,
-					expected: [typeDef.value.value]
-				}];
-			}
-			break;
-		}
-		case 'enum': {
-			if (inferredType.kind === 'literal' && !typeDef.values.some(v => {
-				if (v.attributes) {
-					const enumChildType = {
-						kind: 'literal',
-						value: { kind: typeDef.enumKind ?? (typeof v.value === 'number' ? 'double' : 'string'), value: v.value}
-					} as LiteralType
-					const attributeErrors = v.attributes.flatMap(a => {
-						const h = attributeHandlers[a.name]
-						return h ? h(node, a, inferredType, enumChildType, options) : []
+			return { kind: 'union', members: members };
+		case 'struct':
+			const fields: StructTypeField[] = [];
+			for (const field of typeDef.fields) {
+				if (field.kind === 'pair') {
+					// Don't simplify the value here. We need to have the correct `node` and `parents`, which we
+					// cannot deterministically find for non-string keys.
+					// Instead, this method will be called by every struct child by the outer checking method.
+					fields.push({
+						...field,
+						key: typeof field.key === 'string' ? field.key : simplify(node, field.key, inferredType, options, parents),
 					})
-					if (attributeErrors.length > 0) {
-						return false;
+				} else {
+					const simplified = simplify(node, field.type, inferredType, options, parents);
+					
+					let inner = simplified;
+					// TODO figure out if we can maybe handle attributes in simplify already, so after simplify there is no attributed types
+					// Maybe it would also be worth just attaching an attributes array to type nodes and getting rid of the attributed type alltogether
+					while (inner.kind === 'attributed') {
+						inner = inner.child;
+					}
+
+					if (inner.kind === 'struct') {
+						fields.push(...inner.fields);
 					}
 				}
-				return v.value === inferredType.value.value;
-			})) {
-				return [{
-					kind: "value_mismatch",
-					node: node,
-					expected: typeDef.values.map(v => v.value)
-				}];
 			}
-		}
+			return { kind: 'struct', fields: fields };
+		case 'list': 
+			return { ...typeDef, item: simplify(node, typeDef.item, inferredType, options, parents) }
+		case 'concrete': // TODO
+		case 'template': // TODO
+		default: return typeDef
 	}
-	return errors;
 }
 
 function findStructKey<T extends AstNode>(node: T, typeDefs: (McdocType | string)[], inferredType: McdocType, options: ValidatorOptions<T>, parents: T[]): McdocType | string | undefined {
