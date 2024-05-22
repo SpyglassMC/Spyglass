4254b3d09c38a481055bdd61f00916e0cd1909cc
Calverin <yosefs646@gmail.com>
2024-05-22 17:05:44 -0400

    Cleaning up and random $ fixes

--- packages/mcfunction/src/parser/macro.ts
+++ packages/mcfunction/src/parser/macro.ts
@@ -33,70 +33,32 @@ export function macro(): core.Parser<MacroNode> {
 
         let beginning = src.cursor
         let txt = src.readUntil('$', '\r', '\n')
-        let first = true
-        while (txt.length > 0 || first) {
+        do { // Do-while because we always want it to work the first time
             let wasMacro = false
-            if (txt.substring(0, 2) === '$(') { // This is a macro key
-                txt += src.read()
-
-                // Error handling
-                const check = txt.substring(2, txt.length - 1)
-                const matchedInvalid = check.replace(/[a-zA-Z0-9_]*/, "")
-                // Invalid key
-                if (matchedInvalid.length > 0) {
-                    ctx.err.report(
-                        localize(
-                            'parser.resource-location.illegal',
-                            matchedInvalid.charAt(0),
-                        ),
-                        core.Range.create(beginning, src.cursor),
-                    )
-                }
-                // No key
-                if (check.length === 0) {
-                    ctx.err.report(
-                        localize('expected', localize('macro-key')),
-                        core.Range.create(beginning, src.cursor),
-                    )
-                }
-                // Uncompleted Macro
-                if (txt.charAt(txt.length - 1) !== ')') {
-                    ctx.err.report(
-                        localize('expected', localeQuote(')')),
-                        core.Range.create(beginning, src.cursor),
-                    )
-                }
-
-                const key: MacroKeyNode = {
-                    type: 'mcfunction:macro_key',
-                    range: core.Range.create(beginning + 2, src.cursor - 1),
-                    colorTokenType: 'property', // Light Blue
-                    key: check,
-                }
-
-                const ans: MacroChildNode = {
-                    type: 'mcfunction:macro_child',
-                    range: core.Range.create(beginning, src.cursor),
-                    options: {
-                        type: 'macro',
-                        colorTokenType: 'literal', // Blue
-                    },
-                    value: txt,
-                    children: [key],
-                }
-                children.push(ans)
+            const child: MacroChildNode = parse(src, ctx, txt, beginning, wasMacro)
+            if (child.options.type === 'macro') {
                 wasMacro = true
-            } else { // This is the rest of the line
-                const ans: MacroChildNode = {
-                    type: 'mcfunction:macro_child',
-                    range: core.Range.create(beginning, src.cursor),
-                    options: {
-                        type: 'other',
-                        colorTokenType: 'string', // Orange
-                    },
-                    value: txt,
+            }
+            // Handle a non-macro $ in the middle of the line
+            let subMacro: MacroChildNode | undefined
+            if (child.options.type === 'other' && child.value?.indexOf('$') !== -1) {
+                const parts: String[] = child.value?.split('$') ?? []
+                let other = ''
+                for (const part of parts) {
+                    if (part.charAt(0) == '(') { // We've found our macro key!
+                        subMacro = parse(src, ctx, '$' + part, beginning + other.length - 1, wasMacro)
+                        child.range.end -= (part.length + 1)
+                    } else { // Keep it in the normal string
+                        other += part + '$'
+                    }
+
                 }
-                children.push(ans)
+                wasMacro = true
+                child.value = other.substring(0, other.length - 1) // Remove the last '$'
+            }
+            children.push(child)
+            if (subMacro) {
+                children.push(subMacro)
             }
             // Prepare for the next block
             beginning = src.cursor
@@ -105,14 +67,11 @@ export function macro(): core.Parser<MacroNode> {
             } else {
                 txt = src.readUntil('\r', '\n', ')')
             }
-            first = false
-        }
+        } while (txt.length > 0)
 
+        // No actual macro
         if (children.length < 3) {
-            ctx.err.report(
-                localize('expected', localize('macro')),
-                core.Range.create(start, src.cursor),
-            )
+            ctx.err.report(localize('expected', localize('macro')), core.Range.create(start, src.cursor))
         }
 
         // Return the result
@@ -126,4 +85,53 @@ export function macro(): core.Parser<MacroNode> {
         ctx.logger.info(children)
 		return ans
 	}
+}
+
+function parse(src: core.Source, ctx: core.ParserContext, txt: String, beginning: number, wasMacro: Boolean): MacroChildNode {
+    if (txt.substring(0, 2) === '$(') { // This is a macro key
+        txt += src.read()
+
+        // Error handling
+        const check = txt.substring(2, txt.length - 1)
+        const matchedInvalid = check.replace(/[a-zA-Z0-9_]*/, "")
+        if (matchedInvalid.length > 0) { // Invalid key
+            ctx.err.report(localize('parser.resource-location.illegal', matchedInvalid.charAt(0)),core.Range.create(beginning, src.cursor))
+        }
+        if (check.length === 0) { // No key
+            ctx.err.report(localize('expected', localize('macro-key')), core.Range.create(beginning, src.cursor))
+        }
+        if (txt.charAt(txt.length - 1) !== ')') { // Missing parenthesis
+            ctx.err.report(localize('expected', localeQuote(')')), core.Range.create(beginning, src.cursor))
+        }
+
+        const key: MacroKeyNode = {
+            type: 'mcfunction:macro_key',
+            range: core.Range.create(beginning + 2, src.cursor - 1),
+            colorTokenType: 'property', // Light Blue
+            key: check,
+        }
+
+        const ans: MacroChildNode = {
+            type: 'mcfunction:macro_child',
+            range: core.Range.create(beginning, src.cursor),
+            options: {
+                type: 'macro',
+                colorTokenType: 'literal', // Blue
+            },
+            value: txt,
+            children: [key],
+        }
+        return ans
+    } else { // This is the rest of the line
+        const ans: MacroChildNode = {
+            type: 'mcfunction:macro_child',
+            range: core.Range.create(beginning, src.cursor),
+            options: {
+                type: 'other',
+                colorTokenType: 'string', // Orange
+            },
+            value: txt,
+        }
+        return ans
+    }
 }
