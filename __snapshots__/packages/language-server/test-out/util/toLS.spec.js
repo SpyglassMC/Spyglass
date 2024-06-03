a6036c6c5da921658abbc41b6cd836cb9bdab73e
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-06-02 20:49:57 -0400

    ✅ Add very basic unit tests for `toLS.semanticTokens()` - not yet testing the behavior of shimming multiline token support

---
+++ __snapshots__/packages/language-server/test-out/util/toLS.spec.js
@@ -0,0 +1,29 @@
+exports['semanticTokens Tokenize "foo" 1'] = [
+  {
+    "deltaLine": 0,
+    "deltaStartChar": 0,
+    "length": 100,
+    "tokenType": 0,
+    "tokenModifiers": 0
+  }
+]
+
+exports['semanticTokens Tokenize "foo↓bar" 1'] = [
+  {
+    "deltaLine": 0,
+    "deltaStartChar": 0,
+    "length": 100,
+    "tokenType": 0,
+    "tokenModifiers": 0
+  }
+]
+
+exports['semanticTokens Tokenize "foo↓bar↓qux" 1'] = [
+  {
+    "deltaLine": 0,
+    "deltaStartChar": 0,
+    "length": 100,
+    "tokenType": 0,
+    "tokenModifiers": 0
+  }
+]
---
+++ packages/language-server/test/util/toLS.spec.ts
@@ -0,0 +1,63 @@
+import type * as core from '@spyglassmc/core'
+import { showWhitespaceGlyph } from '@spyglassmc/core/test-out/utils.js'
+import snapshot from 'snap-shot-it'
+import { TextDocument } from 'vscode-languageserver-textdocument'
+import type * as ls from 'vscode-languageserver/node.js'
+import { semanticTokens } from '../../lib/util/toLS.js'
+
+/**
+ * The result of decoding a semantic token from an integer list.
+ * The VSCode API documentation details what the integer list represents here:
+ * https://code.visualstudio.com/api/references/vscode-api#DocumentSemanticTokensProvider.provideDocumentSemanticTokens
+ */
+interface DecodedSemanticToken {
+	deltaLine: number
+	deltaStartChar: number
+	length: number
+	tokenType: number
+	tokenModifiers: number
+}
+const decodeSemanticTokens = (
+	tokens: ls.SemanticTokens['data'],
+): DecodedSemanticToken[] => {
+	if (tokens.length % 5 !== 0) {
+		throw new Error('Array of semantic tokens must be divisible by 5')
+	}
+	const decodedTokens = []
+	for (let i = 0; i < tokens.length; i += 5) {
+		const decodedToken = {
+			deltaLine: tokens[i],
+			deltaStartChar: tokens[i + 1],
+			length: tokens[i + 2],
+			tokenType: tokens[i + 3],
+			tokenModifiers: tokens[i + 4],
+		}
+		decodedTokens.push(decodedToken)
+	}
+	return decodedTokens
+}
+
+describe('semanticTokens', () => {
+	const tokens: core.ColorToken[] = [
+		{
+			range: {
+				start: 0,
+				end: 100,
+			},
+			type: 'comment',
+		},
+	]
+	const suites: { content: string }[] = [
+		{ content: 'foo' },
+		{ content: 'foo\nbar' },
+		{ content: 'foo\nbar\nqux' },
+	]
+	for (const { content } of suites) {
+		const doc = TextDocument.create('file:///test', '', 0, content)
+		const itTitle = `Tokenize "${showWhitespaceGlyph(content)}"`
+		it(itTitle, () => {
+			const { data } = semanticTokens(tokens, doc, true)
+			snapshot(decodeSemanticTokens(data))
+		})
+	}
+})
