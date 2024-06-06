9ac43ea354b3d8d11d4ec77c817ec0be486f5c9a
Misode <misoloo64@gmail.com>
2024-06-06 19:35:07 +0200

    Refactor completer

--- packages/json/src/completer/index.ts
+++ packages/json/src/completer/index.ts
@@ -1,54 +1,50 @@
-import type * as core from '@spyglassmc/core'
+import * as core from '@spyglassmc/core'
 import * as mcdoc from '@spyglassmc/mcdoc'
-import type {
-	JsonArrayNode,
-	JsonBooleanNode,
-	JsonNode,
-	JsonObjectNode,
-} from '../node/index.js'
+import type { JsonArrayNode, JsonObjectNode } from '../node/index.js'
 import { JsonStringNode } from '../node/index.js'
 
-export const entry: core.Completer<JsonNode> = (node, ctx) => {
-	if (!node.typeDef) return []
-	return mcdoc.runtime.completer.entry<JsonNode>(node.typeDef, {
-		matchesType: (node, def) => {
-			switch (node.type) {
-				case 'json:object':
-					return def.kind === 'struct'
-				case 'json:array':
-					return [
-						'list',
-						'tuple',
-						'byte_array',
-						'int_array',
-						'long_array',
-					]
-						.includes(def.kind)
-				case 'json:string':
-					return def.kind === 'string'
-				case 'json:number':
-					return ['byte', 'short', 'int', 'long', 'float', 'double']
-						.includes(def.kind)
-				case 'json:boolean':
-					return def.kind === 'boolean'
-				case 'json:null':
-					return false
-			}
+const array: core.Completer<JsonArrayNode> = (node, ctx) => {
+	const index = core.binarySearch(node.children, ctx.offset, (n, o) => {
+		return core.Range.compareOffset(n.range, o, true)
+	})
+	const item = index >= 0 ? node.children[index] : undefined
+	if (!item || !core.ItemNode.is(item) || !item.value) {
+		// TODO: figure out how to autocomplete when there is no item node
+		return []
+	}
+	return ctx.meta.getCompleter(item.value.type)(item.value, ctx)
+}
+
+const object: core.Completer<JsonObjectNode> = (node, ctx) => {
+	return core.completer.record({
+		key: (_r, pair, _c, range, _iv, _ip, exitingKeys) => {
+			if (!node.typeDef) return []
+			const keyRange = pair?.key?.range ?? range
+			const keySet = new Set(exitingKeys.flatMap(n => {
+				return JsonStringNode.is(n) ? [n.value] : []
+			}))
+			return mcdoc.runtime.completer.getFields(node.typeDef)
+				.filter(({ key }) => !keySet.has(key))
+				.map(({ key, field }) =>
+					core.CompletionItem.create(key, keyRange, {
+						kind: core.CompletionKind.Field,
+						detail: mcdoc.McdocType.toString(
+							field.type as core.Mutable<mcdoc.McdocType>,
+						),
+						sortText: field.optional ? 'z' : 'a',
+						filterText: `"${key}"`,
+						insertText: `"${key}": `,
+					})
+				)
 		},
-		getString: (node) => {
-			return JsonStringNode.is(node) ? node.value : undefined
+		value: (_r, pair, ctx) => {
+			if (!pair.value) return []
+			return ctx.meta.getCompleter(pair.value.type)(pair.value, ctx)
 		},
-		mapField: (item) => ({
-			...item,
-			filterText: `"${item.label}"`,
-			insertText: `"${item.label}": `,
-		}),
-	})(node, ctx)
+	})(node as any, ctx)
 }
 
 export function register(meta: core.MetaRegistry): void {
-	meta.registerCompleter<JsonArrayNode>('json:array', entry)
-	meta.registerCompleter<JsonBooleanNode>('json:boolean', entry)
-	meta.registerCompleter<JsonObjectNode>('json:object', entry)
-	meta.registerCompleter<JsonStringNode>('json:string', entry)
+	meta.registerCompleter<JsonArrayNode>('json:array', array)
+	meta.registerCompleter<JsonObjectNode>('json:object', object)
 }
