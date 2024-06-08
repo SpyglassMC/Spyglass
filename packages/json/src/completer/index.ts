5ed7232e09aae51d3ea8bc104878471a5196a781
Misode <misoloo64@gmail.com>
2024-06-08 04:50:03 +0200

    Add value completions + improve list and object completers

--- packages/json/src/completer/index.ts
+++ packages/json/src/completer/index.ts
@@ -1,6 +1,11 @@
 import * as core from '@spyglassmc/core'
 import * as mcdoc from '@spyglassmc/mcdoc'
-import type { JsonArrayNode, JsonObjectNode } from '../node/index.js'
+import type {
+	JsonArrayNode,
+	JsonBooleanNode,
+	JsonNumberNode,
+	JsonObjectNode,
+} from '../node/index.js'
 import { JsonStringNode } from '../node/index.js'
 
 const array: core.Completer<JsonArrayNode> = (node, ctx) => {
@@ -8,43 +13,88 @@ const array: core.Completer<JsonArrayNode> = (node, ctx) => {
 		return core.Range.compareOffset(n.range, o, true)
 	})
 	const item = index >= 0 ? node.children[index] : undefined
-	if (!item || !core.ItemNode.is(item) || !item.value) {
-		// TODO: figure out how to autocomplete when there is no item node
-		return []
+	if (item?.value) {
+		return ctx.meta.getCompleter(item.value.type)(item.value, ctx)
 	}
-	return ctx.meta.getCompleter(item.value.type)(item.value, ctx)
+	if (node.typeDef?.kind === 'list') {
+		const completions = getValues(node.typeDef.item, ctx.offset, ctx)
+		if (
+			ctx.offset < node.children[node.children.length - 1]?.range.start ?? 0
+		) {
+			return completions.map(c => ({ ...c, insertText: c.insertText + ',' }))
+		}
+		return completions
+	}
+	return []
 }
 
 const object: core.Completer<JsonObjectNode> = (node, ctx) => {
 	return core.completer.record({
-		key: (_r, pair, _c, range, _iv, _ip, exitingKeys) => {
+		key: (_r, pair, _c, range, iv, ipe, exitingKeys) => {
 			if (!node.typeDef) return []
-			const keyRange = pair?.key?.range ?? range
 			const keySet = new Set(exitingKeys.flatMap(n => {
 				return JsonStringNode.is(n) ? [n.value] : []
 			}))
 			return mcdoc.runtime.completer.getFields(node.typeDef)
 				.filter(({ key }) => !keySet.has(key))
 				.map(({ key, field }) =>
-					core.CompletionItem.create(key, keyRange, {
+					core.CompletionItem.create(key, pair?.key ?? range, {
 						kind: core.CompletionKind.Field,
 						detail: mcdoc.McdocType.toString(
 							field.type as core.Mutable<mcdoc.McdocType>,
 						),
-						sortText: field.optional ? '$b' : '$a', // sort before hardcoded $schema
+						sortText: field.optional ? '$b' : '$a', // sort above hardcoded $schema
 						filterText: `"${key}"`,
-						insertText: `"${key}"${pair?.value ? '' : ': '}`,
+						insertText: `"${key}"${iv ? ': ' : ''}${ipe ? '$1,' : ''}`,
 					})
 				)
 		},
-		value: (_r, pair, ctx) => {
-			if (!pair.value) return []
-			return ctx.meta.getCompleter(pair.value.type)(pair.value, ctx)
+		value: (_r, pair, ctx, range) => {
+			if (pair.value) {
+				return ctx.meta.getCompleter(pair.value.type)(pair.value, ctx)
+			}
+			if (pair.key && JsonStringNode.is(pair.key) && node.typeDef) {
+				const pairKey = pair.key.value
+				const field = mcdoc.runtime.completer.getFields(node.typeDef)
+					.find(({ key }) => key === pairKey)
+					?.field.type
+				if (field) {
+					return getValues(field, range, ctx)
+				}
+			}
+			return []
 		},
 	})(node as any, ctx)
 }
 
+const primitive: core.Completer<
+	JsonStringNode | JsonNumberNode | JsonBooleanNode
+> = (node, ctx) => {
+	if (!node.typeDef) return []
+	const range = core.Range.contains(node, ctx.offset, true) ? node : ctx.offset
+	return getValues(node.typeDef, range, ctx)
+}
+
+function getValues(
+	typeDef: core.DeepReadonly<mcdoc.McdocType>,
+	range: core.RangeLike,
+	ctx: core.CompleterContext,
+): core.CompletionItem[] {
+	return mcdoc.runtime.completer.getValues(typeDef, ctx)
+		.map(({ value, detail, kind }) =>
+			core.CompletionItem.create(value, range, {
+				kind: core.CompletionKind.Value,
+				detail: detail,
+				filterText: kind === 'string' ? `"${value}"` : value,
+				insertText: kind === 'string' ? `"${value}"` : value,
+			})
+		)
+}
+
 export function register(meta: core.MetaRegistry): void {
 	meta.registerCompleter<JsonArrayNode>('json:array', array)
+	meta.registerCompleter<JsonBooleanNode>('json:boolean', primitive)
+	meta.registerCompleter<JsonNumberNode>('json:number', primitive)
 	meta.registerCompleter<JsonObjectNode>('json:object', object)
+	meta.registerCompleter<JsonStringNode>('json:string', primitive)
 }
--- packages/mcdoc/src/runtime/completer/index.ts
+++ packages/mcdoc/src/runtime/completer/index.ts
@@ -1,18 +1,61 @@
 import type * as core from '@spyglassmc/core'
-import type { McdocType } from '../../type/index.js'
+import { TypeDefSymbolData } from '../../binder/index.js'
+import type { McdocType, StructTypePairField } from '../../type/index.js'
 
-export function getFields(typeDef: core.DeepReadonly<McdocType>) {
-	if (typeDef.kind !== 'struct') return []
-	return typeDef.fields
-		.flatMap(field => {
-			// TODO: handle attributes
-			if (field.kind === 'spread') return []
-			if (typeof field.key === 'string') {
-				return [{ key: field.key, field }]
-			}
-			if (field.key.kind === 'literal') {
-				return [{ key: `${field.key.value.value}`, field }]
-			}
+export function getFields(
+	typeDef: core.DeepReadonly<McdocType>,
+): { key: string; field: core.DeepReadonly<StructTypePairField> }[] {
+	// TODO: handle attributes
+	switch (typeDef.kind) {
+		case 'union':
+			return typeDef.members.flatMap(getFields)
+		case 'struct':
+			return typeDef.fields
+				.flatMap(field => {
+					if (field.kind === 'spread') return []
+					if (typeof field.key === 'string') {
+						return [{ key: field.key, field }]
+					}
+					if (field.key.kind === 'literal') {
+						return [{ key: `${field.key.value.value}`, field }]
+					}
+					return []
+				})
+		default:
 			return []
-		})
+	}
+}
+
+export function getValues(
+	typeDef: core.DeepReadonly<McdocType>,
+	ctx: core.CompleterContext,
+): { value: string; detail?: string; kind?: McdocType['kind'] }[] {
+	// TODO: handle attributes
+	switch (typeDef.kind) {
+		case 'union':
+			return typeDef.members.flatMap(m => getValues(m, ctx))
+		case 'reference':
+			if (!typeDef.path) return []
+			const symbol = ctx.symbols.query(ctx.doc, 'mcdoc', typeDef.path)
+			const def = symbol.getData(TypeDefSymbolData.is)?.typeDef
+			if (!def) return []
+			return getValues(def, ctx)
+		case 'literal':
+			if (typeDef.value.kind === 'string') {
+				return [{ value: typeDef.value.value, kind: 'string' }]
+			}
+			return [{ value: `${typeDef.value.value}` }]
+		case 'boolean':
+			return ['false', 'true'].map(v => ({ value: v, kind: 'boolean' }))
+		case 'string':
+			return []
+		case 'enum':
+			return typeDef.values.map(v => ({
+				value: `${v.value}`,
+				detail: v.identifier,
+				kind: typeDef.enumKind ?? 'string',
+			}))
+		default:
+			return []
+	}
 }
