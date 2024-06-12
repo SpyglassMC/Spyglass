b69f5d0a3673064bcf243e18ba1cf16b0983f188
Misode <misoloo64@gmail.com>
2024-06-12 03:33:54 +0200

    🐛 Only run checker on json:file nodes

--- packages/java-edition/src/json/checker/index.ts
+++ packages/java-edition/src/json/checker/index.ts
@@ -2,24 +2,20 @@ import type * as core from '@spyglassmc/core'
 import * as json from '@spyglassmc/json'
 import { dissectUri } from '../../binder/index.js'
 
-export const entry: core.Checker<json.JsonNode> = (node, ctx) => {
+export const file: core.Checker<json.JsonFileNode> = (node, ctx) => {
+	const child = node.children[0]
 	if (ctx.doc.uri.endsWith('/pack.mcmeta')) {
-		return json.checker.definition('::java::pack::Pack')(node, ctx)
+		return json.checker.definition('::java::pack::Pack')(child, ctx)
 	}
 	const parts = dissectUri(ctx.doc.uri, ctx)
 	if (parts) {
 		if (parts?.category.startsWith('tag/')) {
-			return json.checker.resource('tag')(node, ctx)
+			return json.checker.resource('tag')(child, ctx)
 		}
-		return json.checker.resource(parts.category)(node, ctx)
+		return json.checker.resource(parts.category)(child, ctx)
 	}
 }
 
 export function register(meta: core.MetaRegistry) {
-	meta.registerChecker<json.JsonNode>('json:array', entry)
-	meta.registerChecker<json.JsonNode>('json:boolean', entry)
-	meta.registerChecker<json.JsonNode>('json:null', entry)
-	meta.registerChecker<json.JsonNode>('json:number', entry)
-	meta.registerChecker<json.JsonNode>('json:object', entry)
-	meta.registerChecker<json.JsonNode>('json:string', entry)
+	meta.registerChecker<json.JsonFileNode>('json:file', file)
 }
