deb27e1c553264e92e4e45f4ad15ac088fc0b9e8
Misode <misoloo64@gmail.com>
2024-06-20 04:52:22 +0200

    Add prefixed node and parser

---
+++ packages/core/test/parser/prefixed.spec.ts
@@ -0,0 +1,23 @@
+import { describe, it } from 'mocha'
+import snapshot from 'snap-shot-it'
+import type { BooleanNode } from '../../lib/index.js'
+import { boolean } from '../../lib/index.js'
+import { type Options, prefixed } from '../../lib/parser/prefixed.js'
+import { showWhitespaceGlyph, testParser } from '../utils.js'
+
+describe.only('prefixed()', () => {
+	const options: Options<BooleanNode> = { prefix: '!', child: boolean }
+	const cases: { content: string }[] = [
+		{ content: '' },
+		{ content: 'false' },
+		{ content: '!false' },
+		{ content: '!test' },
+		{ content: '!!true' },
+	]
+	for (const { content } of cases) {
+		it(`Parse "${showWhitespaceGlyph(content)}"`, () => {
+			const parser = prefixed(options)
+			snapshot(testParser(parser, content))
+		})
+	}
+})
