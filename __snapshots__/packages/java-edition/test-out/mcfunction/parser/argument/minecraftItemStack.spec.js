2da7d4bc8c12c315fcc1485b24344528e30f3228
Misode <misoloo64@gmail.com>
2024-06-20 02:23:16 +0200

    Add a few tests for the new item stack and predicate syntax

--- __snapshots__/packages/java-edition/test-out/mcfunction/parser/argument/minecraftItemStack.spec.js
+++ __snapshots__/packages/java-edition/test-out/mcfunction/parser/argument/minecraftItemStack.spec.js
@@ -1,3 +1,166 @@
+exports['mcfunction argument minecraft:item_stack Parse "apple[!food]" in version 1.20.5 1'] = {
+  "node": {
+    "type": "mcfunction:item_stack",
+    "range": {
+      "start": 0,
+      "end": 12
+    },
+    "id": {
+      "type": "resource_location",
+      "range": {
+        "start": 0,
+        "end": 5
+      },
+      "path": [
+        "apple"
+      ]
+    },
+    "components": {
+      "type": "mcfunction:component_list",
+      "range": {
+        "start": 5,
+        "end": 12
+      },
+      "children": [
+        {
+          "type": "pair",
+          "range": {
+            "start": 6,
+            "end": 11
+          },
+          "children": [
+            {
+              "type": "resource_location",
+              "range": {
+                "start": 6,
+                "end": 11
+              },
+              "path": [
+                "!food"
+              ]
+            }
+          ],
+          "key": {
+            "type": "resource_location",
+            "range": {
+              "start": 6,
+              "end": 11
+            },
+            "path": [
+              "!food"
+            ]
+          }
+        }
+      ]
+    }
+  },
+  "errors": [
+    {
+      "range": {
+        "start": 6,
+        "end": 11
+      },
+      "message": "Illegal character(s): “!”",
+      "severity": 3
+    },
+    {
+      "range": {
+        "start": 11,
+        "end": 11
+      },
+      "message": "Expected “=”",
+      "severity": 3
+    },
+    {
+      "range": {
+        "start": 11,
+        "end": 11
+      },
+      "message": "Expected a value",
+      "severity": 3
+    }
+  ]
+}
+
+exports['mcfunction argument minecraft:item_stack Parse "diamond_pickaxe[unbreakable={}]" in version 1.20.5 1'] = {
+  "node": {
+    "type": "mcfunction:item_stack",
+    "range": {
+      "start": 0,
+      "end": 31
+    },
+    "id": {
+      "type": "resource_location",
+      "range": {
+        "start": 0,
+        "end": 15
+      },
+      "path": [
+        "diamond_pickaxe"
+      ]
+    },
+    "components": {
+      "type": "mcfunction:component_list",
+      "range": {
+        "start": 15,
+        "end": 31
+      },
+      "children": [
+        {
+          "type": "pair",
+          "range": {
+            "start": 16,
+            "end": 30
+          },
+          "children": [
+            {
+              "type": "resource_location",
+              "range": {
+                "start": 16,
+                "end": 27
+              },
+              "path": [
+                "unbreakable"
+              ]
+            },
+            {
+              "type": "nbt:compound",
+              "range": {
+                "start": 28,
+                "end": 30
+              },
+              "children": []
+            }
+          ],
+          "key": {
+            "type": "resource_location",
+            "range": {
+              "start": 16,
+              "end": 27
+            },
+            "path": [
+              "unbreakable"
+            ]
+          },
+          "sep": {
+            "start": 27,
+            "end": 28
+          },
+          "value": {
+            "type": "nbt:compound",
+            "range": {
+              "start": 28,
+              "end": 30
+            },
+            "children": []
+          }
+        }
+      ]
+    }
+  },
+  "errors": []
+}
+
 exports['mcfunction argument minecraft:item_stack Parse "minecraft:stick" 1'] = {
   "node": {
     "type": "mcfunction:item_stack",
@@ -21,6 +184,27 @@ exports['mcfunction argument minecraft:item_stack Parse "minecraft:stick" 1'] = 
 }
 
 exports['mcfunction argument minecraft:item_stack Parse "stick" 1'] = {
+  "node": {
+    "type": "mcfunction:item_stack",
+    "range": {
+      "start": 0,
+      "end": 5
+    },
+    "id": {
+      "type": "resource_location",
+      "range": {
+        "start": 0,
+        "end": 5
+      },
+      "path": [
+        "stick"
+      ]
+    }
+  },
+  "errors": []
+}
+
+exports['mcfunction argument minecraft:item_stack Parse "stick" in version 1.20.5 1'] = {
   "node": {
     "type": "mcfunction:item_stack",
     "range": {
