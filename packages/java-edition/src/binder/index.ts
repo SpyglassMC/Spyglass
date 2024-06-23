31fffdd2f6b814f228328a6ad38aefaf8d6b7807
Misode <misoloo64@gmail.com>
2024-06-24 00:08:03 +0200

    Refactor java edition binder

--- packages/java-edition/src/binder/index.ts
+++ packages/java-edition/src/binder/index.ts
@@ -1,7 +1,6 @@
 import type {
 	FileCategory,
 	RootUriString,
-	TagFileCategory,
 	TaggableResourceLocationCategory,
 	UriBinder,
 	UriBinderContext,
@@ -9,149 +8,141 @@ import type {
 import { fileUtil, TaggableResourceLocationCategories } from '@spyglassmc/core'
 import { ReleaseVersion } from '../dependency/index.js'
 
-interface ScopedVersion {
+interface Resource {
+	path: string
+	category: FileCategory
+	ext: `.${string}`
 	since?: ReleaseVersion
 	until?: ReleaseVersion
 }
 
-interface CategoryDef extends ScopedVersion {
-	category: FileCategory | TagFileCategory
-	extname: string
-	pack: 'data_pack'
-}
-function resource(
-	pack: 'data_pack',
-	category: FileCategory | TagFileCategory,
-	path: string | ScopedVersion,
-	version?: ScopedVersion,
-): [string, CategoryDef] {
-	if (typeof path !== 'string') {
-		// Shift parameters if path is a scoped version
-		version = path
-		path = category
-	}
-	return [path, { pack, category, extname: '.json', ...version }]
+interface ResourceTree {
+	children: Map<string, ResourceTree>
+	resource?: Resource
 }
 
-function dataPackResource(
-	category: FileCategory | TagFileCategory,
-	path: string | ScopedVersion,
-	version?: ScopedVersion,
-) {
-	return resource('data_pack', category, path, version)
-}
+const rootNode: ResourceTree = { children: new Map() }
 
-export const Categories = (() => {
-	const NonTaggableRegistries = new Set<TaggableResourceLocationCategory>([
-		'block',
-		'fluid',
-		'function',
-		'game_event',
-		'item', // They have the legacy `tags/${plural}` path until 1.21.
-		'worldgen/block_placer_type',
-		'worldgen/surface_builder', // TODO: They are removed at some point.
-	])
-
-	const ans = new Map<string, CategoryDef>([
-		// ---------
-		// Pre-1.21 Data Pack plurals
-		// ---------
-		dataPackResource('advancement', 'advancements', { until: '1.21' }),
-		['functions', {
-			category: 'function',
-			extname: '.mcfunction',
-			pack: 'data_pack',
-			until: '1.21',
-		}],
-		dataPackResource('item_modifier', 'item_modifiers', { since: '1.17', until: '1.21' }),
-		dataPackResource('loot_table', 'loot_tables', { until: '1.21' }),
-		dataPackResource('predicate', 'predicates', { until: '1.21' }),
-		dataPackResource('recipe', 'recipes', { until: '1.21' }),
-		['structures', { category: 'structure', extname: '.nbt', pack: 'data_pack', until: '1.21' }],
-		dataPackResource('tag/block', 'tags/blocks', { until: '1.21' }),
-		dataPackResource('tag/entity_type', 'tags/entity_types', { until: '1.21' }),
-		dataPackResource('tag/fluid', 'tags/fluids', { until: '1.21' }),
-		dataPackResource('tag/function', 'tags/functions', { until: '1.21' }),
-		dataPackResource('tag/game_event', 'tags/game_events', { since: '1.17', until: '1.21' }),
-		dataPackResource('tag/item', 'tags/items', { until: '1.21' }),
-		// ---------
-
-		// ---------
-		// Post-1.21 Data Pack non-plurals
-		// ---------
-		dataPackResource('advancement', { since: '1.21' }),
-		['function', {
-			category: 'function',
-			extname: '.mcfunction',
-			pack: 'data_pack',
-			since: '1.21',
-		}],
-		dataPackResource('item_modifier', { since: '1.21' }),
-		dataPackResource('loot_table', { since: '1.21' }),
-		dataPackResource('predicate', { since: '1.21' }),
-		dataPackResource('recipe', { since: '1.21' }),
-		['structure', { category: 'structure', extname: '.nbt', pack: 'data_pack', since: '1.21' }],
-		dataPackResource('tag/block', 'tags/block', { since: '1.21' }),
-		dataPackResource('tag/entity_type', 'tags/entity_type', { since: '1.21' }),
-		dataPackResource('tag/fluid', 'tags/fluid', { since: '1.21' }),
-		dataPackResource('tag/function', 'tags/function', { since: '1.21' }),
-		dataPackResource('tag/game_event', 'tags/game_event', { since: '1.21' }),
-		dataPackResource('tag/item', 'tags/item', { since: '1.21' }),
-		// ---------
-
-		// ---------
-		// Data Pack
-		// ---------
-		dataPackResource('banner_pattern', { since: '1.20.5' }),
-		dataPackResource('chat_type', { since: '1.19' }),
-		dataPackResource('damage_type', { since: '1.19.4' }),
-		dataPackResource('enchantment', { since: '1.21' }),
-		dataPackResource('enchantment_provider', { since: '1.21' }),
-		dataPackResource('jukebox_song', { since: '1.21' }),
-		dataPackResource('painting_variant', { since: '1.21' }),
-		dataPackResource('trim_pattern', { since: '1.19.4' }),
-		dataPackResource('trim_material', { since: '1.19.4' }),
-		dataPackResource('wolf_variant', { since: '1.20.5' }),
-		// ---------
-
-		// ---------
-		// Worldgen
-		// ---------
-		dataPackResource('dimension', { since: '1.16' }),
-		dataPackResource('dimension_type', { since: '1.16' }),
-		dataPackResource('worldgen/biome', { since: '1.16.2' }),
-		dataPackResource('worldgen/configured_carver', { since: '1.16.2' }),
-		dataPackResource('worldgen/configured_feature', { since: '1.16.2' }),
-		dataPackResource('worldgen/configured_structure_feature', { since: '1.16.2', until: '1.19' }),
-		dataPackResource('worldgen/density_function', { since: '1.18.2' }),
-		dataPackResource('worldgen/flat_level_generator_preset', { since: '1.19' }),
-		dataPackResource('worldgen/multi_noise_biome_source_parameter_list', { since: '1.19.4' }),
-		dataPackResource('worldgen/noise', { since: '1.18' }),
-		dataPackResource('worldgen/noise_settings', { since: '1.16.2' }),
-		dataPackResource('worldgen/placed_feature', { since: '1.18' }),
-		dataPackResource('worldgen/processor_list', { since: '1.16.2' }),
-		dataPackResource('worldgen/configured_surface_builder', { since: '1.16.2', until: '1.18' }),
-		dataPackResource('worldgen/structure', { since: '1.19' }),
-		dataPackResource('worldgen/structure_set', { since: '1.18.2' }),
-		dataPackResource('worldgen/template_pool', { since: '1.16.2' }),
-		dataPackResource('worldgen/world_preset', { since: '1.19' }),
-		// ---------
-	])
-
-	for (const registry of TaggableResourceLocationCategories) {
-		if (NonTaggableRegistries.has(registry)) {
-			continue
+function findResource(parts: string[]) {
+	let node: ResourceTree | undefined = rootNode
+	let resource: Resource | undefined = undefined
+	let matchIndex = 0
+	for (let i = 0; i < parts.length; i += 1) {
+		node = node.children.get(parts[i])
+		if (node === undefined) {
+			break
 		}
-		ans.set(`tags/${registry}`, {
-			category: `tag/${registry}` as TagFileCategory,
-			extname: '.json',
-			pack: 'data_pack',
-			since: '1.18',
-		})
+		if (node.resource) {
+			resource = node.resource
+			matchIndex = i + 1
+		}
 	}
+	return {
+		resource,
+		path: parts.slice(matchIndex),
+	}
+}
 
-	return ans
-})()
+function resource(path: string, resource: Partial<Resource> & { category: FileCategory }): void
+function resource(path: FileCategory, resource: Partial<Resource>): void
+function resource(path: string, resource: Partial<Resource> = {}) {
+	let node: ResourceTree | undefined = rootNode
+	for (const part of path.split('/')) {
+		let child: ResourceTree | undefined = node.children.get(part)
+		if (child === undefined) {
+			child = { children: new Map() }
+			node.children.set(part, child)
+		}
+		node = child
+	}
+	node.resource = {
+		path,
+		category: resource.category ?? path as FileCategory,
+		ext: resource.ext ?? '.json',
+		...resource,
+	}
+}
+
+// Pre-1.21 data pack plurals
+resource('advancements', { category: 'advancement', until: '1.21' })
+resource('functions', { category: 'function', until: '1.21', ext: '.mcfunction' })
+resource('item_modifiers', { category: 'item_modifier', since: '1.17', until: '1.21' })
+resource('loot_tables', { category: 'loot_table', until: '1.21' })
+resource('predicates', { category: 'predicate', until: '1.21' })
+resource('recipes', { category: 'recipe', until: '1.21' })
+resource('structures', { category: 'structure', until: '1.21', ext: '.nbt' })
+resource('tags/blocks', { category: 'tag/block', until: '1.21' })
+resource('tags/entity_types', { category: 'tag/entity_type', until: '1.21' })
+resource('tags/fluids', { category: 'tag/fluid', until: '1.21' })
+resource('tags/functions', { category: 'tag/function', until: '1.21' })
+resource('tags/game_events', { category: 'tag/game_event', since: '1.17', until: '1.21' })
+resource('tags/items', { category: 'tag/item', until: '1.21' })
+
+// Post-1.21 data pack non-plurals
+resource('advancement', { since: '1.21' })
+resource('function', { since: '1.21', ext: '.mcfunction' })
+resource('item_modifier', { since: '1.21' })
+resource('loot_table', { since: '1.21' })
+resource('predicate', { since: '1.21' })
+resource('recipe', { since: '1.21' })
+resource('structure', { since: '1.21', ext: '.nbt' })
+resource('tags/block', { category: 'tag/block', since: '1.21' })
+resource('tags/entity_type', { category: 'tag/entity_type', since: '1.21' })
+resource('tags/fluid', { category: 'tag/fluid', since: '1.21' })
+resource('tags/function', { category: 'tag/function', since: '1.21' })
+resource('tags/game_event', { category: 'tag/game_event', since: '1.21' })
+resource('tags/item', { category: 'tag/item', since: '1.21' })
+
+// Data pack
+resource('banner_pattern', { since: '1.20.5' })
+resource('chat_type', { since: '1.19' })
+resource('damage_type', { since: '1.19.4' })
+resource('enchantment', { since: '1.21' })
+resource('enchantment_provider', { since: '1.21' })
+resource('jukebox_song', { since: '1.21' })
+resource('painting_variant', { since: '1.21' })
+resource('trim_pattern', { since: '1.19.4' })
+resource('trim_material', { since: '1.19.4' })
+resource('wolf_variant', { since: '1.20.5' })
+
+// Worldgen
+resource('dimension', { since: '1.16' })
+resource('dimension_type', { since: '1.16' })
+resource('worldgen/biome', { since: '1.16.2' })
+resource('worldgen/configured_carver', { since: '1.16.2' })
+resource('worldgen/configured_feature', { since: '1.16.2' })
+resource('worldgen/configured_structure_feature', { since: '1.16.2', until: '1.19' })
+resource('worldgen/density_function', { since: '1.18.2' })
+resource('worldgen/flat_level_generator_preset', { since: '1.19' })
+resource('worldgen/multi_noise_biome_source_parameter_list', { since: '1.19.4' })
+resource('worldgen/noise', { since: '1.18' })
+resource('worldgen/noise_settings', { since: '1.16.2' })
+resource('worldgen/placed_feature', { since: '1.18' })
+resource('worldgen/processor_list', { since: '1.16.2' })
+resource('worldgen/configured_surface_builder', { since: '1.16.2', until: '1.18' })
+resource('worldgen/structure', { since: '1.19' })
+resource('worldgen/structure_set', { since: '1.18.2' })
+resource('worldgen/template_pool', { since: '1.16.2' })
+resource('worldgen/world_preset', { since: '1.19' })
+
+// Tags
+const NonTaggableRegistries = new Set<TaggableResourceLocationCategory>([
+	// Legacy plural paths
+	'block',
+	'fluid',
+	'function',
+	'game_event',
+	'item',
+	// Removed before 1.18
+	'worldgen/block_placer_type',
+	'worldgen/surface_builder',
+])
+for (const registry of TaggableResourceLocationCategories) {
+	if (NonTaggableRegistries.has(registry)) {
+		continue
+	}
+	resource(`tags/${registry}`, { category: `tag/${registry}`, since: '1.18' })
+}
 
 export function* getRels(
 	uri: string,
@@ -161,7 +152,7 @@ export function* getRels(
 
 	const parts = uri.split('/')
 	for (let i = parts.length - 2; i >= 0; i--) {
-		if (parts[i] === 'data') {
+		if (parts[i] === 'data') { // TODO: support assets
 			yield parts.slice(i).join('/')
 		}
 	}
@@ -170,52 +161,40 @@ export function* getRels(
 }
 
 export function dissectUri(uri: string, ctx: UriBinderContext) {
-	const regex =
-		/^data\/([^\/]+)\/((?:tags\/)?(?:worldgen\/)?[a-z_]+)\/(.*)(\.(?:mcfunction|json|nbt))$/
-
 	const rels = getRels(uri, ctx.roots)
-	const { customResources } = ctx.config.env
+	const release = ctx.project['loadedVersion'] as ReleaseVersion | undefined
+	if (!release) {
+		return undefined
+	}
 
 	for (const rel of rels) {
-		const match = rel.match(regex)
-		if (!match) {
+		const parts = rel.split('/')
+		const [pack, namespace, ...rest] = parts
+		if (pack !== 'data') {
+			continue // TODO: support assets
+		}
+		const { resource, path } = findResource(rest)
+		if (!resource || path.length === 0) {
 			continue
 		}
-		let def = Categories.get(match[2])
-
-		if (!def && customResources.length > 0) {
-			// Try to match the category for custom resources.
-			const subFolders = match[3].split('/').slice(0, -1)
-
-			for (let i = 0; i < subFolders.length - 1; i++) { // if subFolders is empty, this loop will not run
-				const customCategory = `${match[2]}/${subFolders.slice(0, i + 1).join('/')}`
-
-				def = Categories.get(customCategory)
-
-				// we don't break here because we want to find the most specific category
-			}
-		}
-		if (!def || def.extname !== match[4]) {
+		let identifier = path.join('/')
+		if (!identifier.endsWith(resource.ext)) {
 			continue
 		}
-		const loadedVersion = ctx.project['loadedVersion'] as ReleaseVersion | undefined
-		if (
-			!loadedVersion // FIXME: check why this can be undefined sometimes
-			|| !matchVersion(loadedVersion, def.since, def.until)
-		) {
+		identifier = identifier.slice(0, -resource.ext.length)
+		if (!matchVersion(release, resource.since, resource.until)) {
 			continue
 		}
-		return { category: def.category, namespace: match[1], identifier: match[3] }
+		return { category: resource.category, namespace, identifier }
 	}
 
 	return undefined
 }
 
 export const uriBinder: UriBinder = (uris: readonly string[], ctx: UriBinderContext) => {
-	const { customResources } = ctx.config.env
-	for (const [category, def] of customResources) {
-		if (def.pack === 'data_pack') { // TODO
-			Categories.set(category, def as CategoryDef)
+	for (const [path, config] of Object.entries(ctx.config.env.customResources)) {
+		if (config.pack === 'data') {
+			resource(path, { ...config, category: config.category as FileCategory })
 		}
 	}
 
@@ -234,7 +213,11 @@ function matchVersion(
 	since: ReleaseVersion | undefined,
 	until: ReleaseVersion | undefined,
 ): boolean {
-	if (since && ReleaseVersion.cmp(target, since) < 0) return false
-	if (until && ReleaseVersion.cmp(until, target) <= 0) return false
+	if (since && ReleaseVersion.cmp(target, since) < 0) {
+		return false
+	}
+	if (until && ReleaseVersion.cmp(until, target) <= 0) {
+		return false
+	}
 	return true
 }
