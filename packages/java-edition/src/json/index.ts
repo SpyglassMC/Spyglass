9d73772ab30494902c6a04a46e7ab5a71c8f2ebc
Misode <misoloo64@gmail.com>
2024-07-04 05:43:44 +0200

    Add criterion attribute

--- packages/java-edition/src/json/index.ts
+++ packages/java-edition/src/json/index.ts
@@ -2,7 +2,10 @@
 
 import type * as core from '@spyglassmc/core'
 import * as checker from './checker/index.js'
+import { registerMcdocAttributes } from './mcdocAttributes.js'
 
 export const initialize = (ctx: core.ProjectInitializerContext) => {
+	registerMcdocAttributes(ctx.meta)
+
 	checker.register(ctx.meta)
 }
