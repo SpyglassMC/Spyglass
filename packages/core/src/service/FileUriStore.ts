189022fc4c8eb790e7f05cb72d42ff5fd38dcc0a
SPGoding <i@spgoding.com>
2024-10-12 01:11:10 -0500

    Stash

---
+++ packages/core/src/service/FileUriStore.ts
@@ -0,0 +1,25 @@
+/**
+ * A data structure for storing a list of file URIs efficiently.
+ *
+ * Currently implemented naively with trees.
+ */
+export class FileUriStore {
+	#tree = {}
+
+	add(uri: string) {}
+
+	addAll(store: FileUriStore) {
+	}
+
+	has(uri: string) {
+	}
+
+	delete(uri: string) {
+	}
+
+	getChildrenOf(uri: string) {
+	}
+
+	getDescendentsOf(uri: string) {
+	}
+}
