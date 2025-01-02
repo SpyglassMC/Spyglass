da865cd047280c3ef081deac1c009b7ef6b2b5c9
NeunEinser <daniel30797@gmail.com>
2025-01-02 21:53:13 +0100

    ♻️ Use bigint for long types in mcdoc (#1487)

--- packages/json/src/checker/index.ts
+++ packages/json/src/checker/index.ts
@@ -123,7 +123,15 @@ function inferType(node: JsonNode): Exclude<mcdoc.McdocType, mcdoc.UnionType> {
 		case 'json:number':
 			return {
 				kind: 'literal',
-				value: { kind: node.value.type, value: Number(node.value.value) },
+				value: node.value.type === 'float'
+					? {
+						kind: node.value.type,
+						value: node.value.value,
+					}
+					: {
+						kind: node.value.type,
+						value: node.value.value,
+					},
 			}
 		case 'json:null':
 			return { kind: 'any' } // null is always invalid?
