243d5861285bca65b2e166fd3af9248fc118ac84
Misode <misoloo64@gmail.com>
2025-01-04 02:15:59 +0100

    🐛 Fix hash of binary files in cache service

--- packages/core/src/service/CacheService.ts
+++ packages/core/src/service/CacheService.ts
@@ -58,6 +58,7 @@ interface ValidateResult {
 export class CacheService {
 	checksums = Checksums.create()
 	errors: ErrorCache = {}
+	dirtyFiles = new Set<string>()
 	#hasValidatedFiles = false
 
 	/**
@@ -69,16 +70,7 @@ export class CacheService {
 			if (!this.#hasValidatedFiles) {
 				return
 			}
-			try {
-				// TODO: Don't update this for every single change.
-				this.checksums.files[doc.uri] = await this.project.externals.crypto.getSha1(
-					doc.getText(),
-				)
-			} catch (e) {
-				if (!this.project.externals.error.isKind(e, 'EISDIR')) {
-					this.project.logger.error(`[CacheService#hash-file] ${doc.uri}`)
-				}
-			}
+			this.dirtyFiles.add(doc.uri)
 		})
 		this.project.on('rootsUpdated', async ({ roots }) => {
 			if (!this.#hasValidatedFiles) {
@@ -89,7 +81,7 @@ export class CacheService {
 					this.checksums.roots[root] = await this.project.fs.hash(root)
 				} catch (e) {
 					if (!this.project.externals.error.isKind(e, 'EISDIR')) {
-						this.project.logger.error(`[CacheService#hash-root] ${root}`)
+						this.project.logger.error(`[CacheService#hash-root] ${root}`, e)
 					}
 				}
 			}
@@ -163,7 +155,7 @@ export class CacheService {
 				}
 			} catch (e) {
 				if (!this.project.externals.error.isKind(e, 'EISDIR')) {
-					this.project.logger.error(`[CacheService#hash-file] ${uri}`)
+					this.project.logger.error(`[CacheService#hash-file] ${uri}`, e)
 				}
 				// Failed calculating hash. Assume the root has changed.
 			}
@@ -222,6 +214,19 @@ export class CacheService {
 		let filePath: string | undefined
 		try {
 			filePath = await this.getCacheFileUri()
+
+			for (const uri of this.dirtyFiles) {
+				try {
+					this.checksums.files[uri] = await this.project.fs.hash(uri)
+				} catch (e) {
+					if (!this.project.externals.error.isKind(e, 'EISDIR')) {
+						this.project.logger.error(`[CacheService#hash-file] ${uri}`, e)
+					}
+				}
+			}
+			this.dirtyFiles.clear()
+			__profiler.task('Hash Files')
+
 			const cache: CacheFile = {
 				version: LatestCacheVersion,
 				projectRoots: this.project.projectRoots,
