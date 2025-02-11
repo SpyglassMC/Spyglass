d197c98710f63c9e70e5f9203d1dddb5ded04814
SPGoding <i@spgoding.com>
2025-02-10 22:49:24 -0600

    💬 Add hint for escaping percent signs

---
+++ packages/java-edition/test/json/parser/index.spec.ts
@@ -0,0 +1,25 @@
+import { describe, it } from 'mocha'
+import snapshot from 'snap-shot-it'
+import { translationValueParser } from '../../../lib/json/parser/index.js'
+import { showWhitespaceGlyph, testParser } from '@spyglassmc/core/test-out/utils.js'
+
+describe('translationValueParser()', () => {
+	const cases: string[] = [
+		"hello world",
+		"hello %",
+		"hello %%",
+		"hello %s",
+		"hello %s%",
+		"hello %s%%",
+		"hello %1",
+		"hello %1$",
+		"hello %$s",
+		"hello %1$s",
+		"hello %s %42$s",
+	]
+	for (const content of cases) {
+		it(`Parse "${showWhitespaceGlyph(content)}"`, () => {
+			snapshot(testParser(translationValueParser, content))
+		})
+	}
+})
