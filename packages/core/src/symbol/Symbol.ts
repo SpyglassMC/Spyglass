46ec0cd3ff6edf35bec12540066d8eaf389b5010
NeunEinser <daniel30797@gmail.com>
2025-12-21 22:40:00 +0100

    Serialize and deserialize bigints correctly in SymbolTable

--- packages/core/src/symbol/Symbol.ts
+++ packages/core/src/symbol/Symbol.ts
@@ -568,6 +568,23 @@ export namespace SymbolTable {
 	 * are set properly.
 	 */
 	export function link(table: UnlinkedSymbolTable): SymbolTable {
+		const convertData = (value: any) => {
+			if (value && typeof value === 'object') {
+				if (value['$$type'] === 'bigint' && Object.hasOwn(value, 'value')) {
+					return BigInt(value.value)
+				} else {
+					convertRecordEntries(value)
+				}
+			}
+			return value
+		}
+
+		const convertRecordEntries = (value: Record<string, any>) => {
+			for (const [key, child] of Object.entries(value)) {
+				value[key] = convertData(child)
+			}
+		}
+
 		const linkSymbol = (
 			symbol: Symbol,
 			parentMap: SymbolMap,
@@ -582,6 +599,8 @@ export namespace SymbolTable {
 			if (parentSymbol) {
 				symbol.parentSymbol = parentSymbol
 			}
+			symbol.data = convertData(symbol.data)
+
 			if (symbol.members) {
 				linkSymbolMap(symbol.members, symbol, category, path)
 			}
@@ -614,12 +633,29 @@ export namespace SymbolTable {
 	 * are deleted.
 	 */
 	export function unlink(table: SymbolTable): UnlinkedSymbolTable {
+		const convertData = (value: any) => {
+			if (typeof value === 'bigint') {
+				return { '$$type': 'bigint', value: value.toString() }
+			} else if (value && typeof value === 'object') {
+				convertRecordEntries(value)
+			}
+			return value
+		}
+
+		const convertRecordEntries = (value: Record<string, any>) => {
+			for (const [key, child] of Object.entries(value)) {
+				value[key] = convertData(child)
+			}
+		}
+
 		const unlinkSymbol = (symbol: UnlinkedSymbol) => {
 			delete symbol.category
 			delete symbol.identifier
 			delete symbol.parentMap
 			delete symbol.parentSymbol
 			delete symbol.path
+
+			symbol.data = convertData(symbol.data)
 			if (symbol.members) {
 				unlinkSymbolMap(symbol.members)
 			}
