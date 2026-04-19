6e2eabd1ecd393dbcc4fce365c935c08058263c1
SPGoding <i@spgoding.com>
2026-04-19 00:37:07 -0500

    ✨ Add retries in fetcher

--- packages/core/src/service/fetcher.ts
+++ packages/core/src/service/fetcher.ts
@@ -1,7 +1,12 @@
-import { Dev } from '../common/index.js'
+import { Dev, sleep } from '../common/index.js'
 import type { Externals, Logger } from '../common/index.js'
 
-const FETCH_TIMEOUT_MS = 30_000
+const FETCH_RETRY_BASE_MS = 1_000
+const FETCH_RETRY_MAX_ATTEMPTS = 3
+const FETCH_PER_TRY_TIMEOUT_MS = 10_000
+const FETCH_TOTAL_TIMEOUT_MS = 15_000
+
+const STALE_HEADER = 'spyglassmc-is-stale'
 
 export async function fetchWithCache(
 	{ web }: Externals,
@@ -18,7 +23,7 @@ export async function fetchWithCache(
 	}
 	request.headers.set('User-Agent', 'SpyglassMC (+https://spyglassmc.com)')
 	try {
-		const response = await fetch(request, { signal: AbortSignal.timeout(FETCH_TIMEOUT_MS) })
+		const response = await fetchWithRetries(request)
 		if (response.status === 304) {
 			Dev.assertDefined(cachedResponse)
 			logger.info(`[fetchWithCache] reusing cache for ${request.url}`)
@@ -42,10 +47,52 @@ export async function fetchWithCache(
 		logger.warn('[fetchWithCache] fetch', e)
 		if (cachedResponse) {
 			logger.info(`[fetchWithCache] falling back to cache for ${request.url}`)
-			return cachedResponse
+			// Set the stale header when fallback is used
+			const newHeaders = new Headers(cachedResponse.headers)
+			newHeaders.set(STALE_HEADER, '1')
+			return new Response(cachedResponse.body, {
+				status: cachedResponse.status,
+				statusText: cachedResponse.statusText,
+				headers: newHeaders,
+			})
 		}
 		throw e
 	}
+}
+
+export function isStaleFetcherResponse(response: Response) {
+	return response.headers.has(STALE_HEADER)
+}
+
+async function fetchWithRetries(request: Request) {
+	let lastResult: Response | Error | undefined
+	const totalSignal = AbortSignal.timeout(FETCH_TOTAL_TIMEOUT_MS)
+
+	Dev.assertTrue(FETCH_RETRY_MAX_ATTEMPTS > 0, 'Number of attempts must be greater than 0')
+	for (let i = 0; i < FETCH_RETRY_MAX_ATTEMPTS; i++) {
+		const isLastAttempt = i === FETCH_RETRY_MAX_ATTEMPTS - 1
+		try {
+			lastResult = await fetch(request.clone(), {
+				signal: AbortSignal.any([
+					totalSignal,
+					AbortSignal.timeout(FETCH_PER_TRY_TIMEOUT_MS),
+				]),
+			})
+			if (lastResult.status < 500) {
+				return lastResult
+			}
+		} catch (e) {
+			lastResult = e as Error
+		}
+		if (!isLastAttempt) {
+			await sleep(Math.round(Math.random() * (2 ** i) * FETCH_RETRY_BASE_MS))
+		}
+	}
+
+	if (lastResult instanceof Response) {
+		return lastResult
+	}
+	throw lastResult!
 }
 
 // Fetchr? I hardly know her: https://github.com/NeunEinser/bingo
