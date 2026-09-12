7ff6abfc47bbc023361df958706ef27d2952641b
Dahesor <dahesor@gmail.com>
2026-09-11 21:08:26 -0400

    🐛Fix compound filters skips the next leaf node

--- packages/nbt/src/checker/index.ts
+++ packages/nbt/src/checker/index.ts
@@ -381,7 +381,15 @@ export function path(
 						({ originalNode: link }) => link.node.range,
 					)(error)
 				},
-				attachTypeInfo: (link, definition, desc = '', originalDefinition) => {
+				attachTypeInfo: function attachTypeInfo(
+					link,
+					definition,
+					desc = '',
+					originalDefinition,
+				) {
+					if (NbtPathFilterNode.is(link.node) && link.next) {
+						attachTypeInfo(link.next, definition, desc, originalDefinition)
+					}
 					if (link.node.type === 'leaf') {
 						link.path.endOriginalTypeDef = originalDefinition
 					}
